30da630a5af0e9814afeb6ed84e242be18a98a9c
shitogishi_yujin <hyerk.master@gmail.com>
2022-01-27 02:02:16 +0900

    カードのコンポーネント切り分け

---
+++ web/resources/js/components/ProjectCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-row>
+    <v-card>
+      <v-subheader>{{ card }}</v-subheader>
+
+      <v-list two-line>
+        <template v-for="n in 6">
+          <v-list-item :key="n">
+            <v-list-item-avatar color="grey darken-1"> </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>Message {{ n }}</v-list-item-title>
+
+              <v-list-item-subtitle>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil
+                repellendus distinctio similique
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider v-if="n !== 6" :key="`divider-${n}`" inset></v-divider>
+        </template>
+      </v-list>
+    </v-card>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    card: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
