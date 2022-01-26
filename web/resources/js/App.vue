33fbb9a0004ee1b06094f98b0047b0243344f11c
shitogishi_yujin <hyerk.master@gmail.com>
2022-01-26 20:05:58 +0900

    navbarのコンポーネント切り分け

--- web/resources/js/App.vue
+++ web/resources/js/App.vue
@@ -3,9 +3,22 @@
     <main>
       <div class="container">
         <v-app>
+        <Navbar />
         <RouterView />
         </v-app>
       </div>
     </main>
   </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar,
+  }
+}
+
+
+</script>
