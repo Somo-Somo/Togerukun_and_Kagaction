ed190b39097c78c38533b0ec80edb2ea41f18978
shitogishi_yujin <hyerk.master@gmail.com>
2022-01-26 16:03:58 +0900

    これwebpack.mix.jsにつけないとコンパイルできない

--- web/webpack.mix.js
+++ web/webpack.mix.js
@@ -15,5 +15,5 @@ mix.browserSync({
     proxy: '0.0.0.0:8080', // アプリの起動アドレス
     open: false // ブラウザを自動で開かない
 })
-.js('resources/js/app.js', 'public/js')
+.js('resources/js/app.js', 'public/js').vue()
 .version();
