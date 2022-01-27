a0bd1499162308fea9d62f687dce97c9368b50e5
shitogishi_yujin <hyerk.master@gmail.com>
2022-01-27 17:09:29 +0900

    Navigationbarの模様替え

--- web/resources/js/components/Navbar.vue
+++ web/resources/js/components/Navbar.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="hidden-sm-and-down" style="width: 256px">
-    <v-navigation-drawer v-model="drawer" app>
-      <v-sheet color="grey lighten-4" class="pa-4">
+    <v-navigation-drawer color="teal lighten-3" v-model="drawer" app>
+      <v-sheet class="pa-4" color="teal lighten-3">
         <v-avatar class="mb-4" color="grey darken-1" size="64"></v-avatar>
         <div>yujin@vizhd.com</div>
       </v-sheet>
-      <v-divider></v-divider>
       <v-list>
         <v-list-item v-for="[icon, text] in links" :key="icon" link>
           <v-list-item-icon>
