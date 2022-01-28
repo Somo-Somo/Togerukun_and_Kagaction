62a09f696158ea299603b2566505880d256cee92
shitogishi_yujin <hyerk.master@gmail.com>
2022-01-28 14:48:11 +0900

    ナビゲーションバーのデザイン完成

--- web/resources/js/components/Navbar.vue
+++ web/resources/js/components/Navbar.vue
@@ -1,21 +1,67 @@
 <template>
   <div class="hidden-sm-and-down" style="width: 256px">
-    <v-navigation-drawer color="teal lighten-3" v-model="drawer" app>
-      <v-sheet class="pa-4" color="teal lighten-3">
-        <v-avatar class="mb-4" color="grey darken-1" size="64"></v-avatar>
-        <div>yujin@vizhd.com</div>
+    <v-navigation-drawer color="#80CBC4" v-model="drawer" app>
+      <v-sheet
+        class="d-flex flex-row px-4"
+        color="#80CBC4"
+        style="height: 72px"
+      >
       </v-sheet>
-      <v-list>
-        <v-list-item v-for="[icon, text] in links" :key="icon" link>
-          <v-list-item-icon>
-            <v-icon>{{ icon }}</v-icon>
+      <v-list class="py-4">
+        <v-list-item
+          v-for="[icon, text] in links"
+          :key="icon"
+          class="d-flex px-8"
+          style="height: 48px"
+          link
+        >
+          <v-list-item-icon class="align-self-center mr-6">
+            <v-icon color="teal lighten-5">{{ icon }}</v-icon>
           </v-list-item-icon>
-
-          <v-list-item-content>
+          <v-list-item-content class="align-self-center">
             <v-list-item-title>{{ text }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
+      <v-divider class="mx-6" color="#80CBC4"></v-divider>
+      <v-list class="py-4">
+        <v-subheader class="px-8" style="font-size: 0.75em"
+          >プロジェクト</v-subheader
+        >
+        <v-list-item
+          v-for="[icon, text] in projects"
+          :key="icon"
+          class="d-flex px-8"
+          style="height: 48px"
+          link
+        >
+          <v-list-item-icon class="align-self-center mr-6">
+            <v-icon color="teal lighten-5">{{ icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content class="align-self-center">
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-footer color="#80CBC4" absolute>
+        <v-divider color="#80CBC4"></v-divider>
+        <v-sheet
+          class="d-flex flex-row justify-space-around pl-2"
+          style="height: 72px"
+          color="#80CBC4"
+        >
+          <v-list-item-avatar
+            color="grey darken-1"
+            size="36"
+          ></v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>四戸岸 勇仁</v-list-item-title>
+            <v-list-item-subtitle style="font-size: 0.75em">
+              y.shitogishi@vizhd.co.jp
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-sheet>
+      </v-footer>
     </v-navigation-drawer>
   </div>
 </template>
@@ -24,15 +70,19 @@
 export default {
   props: {
     drawer: {
-        type: Boolean
-    }
+      type: Boolean,
+    },
   },
   data: () => ({
     links: [
-      ["mdi-inbox-arrow-down", "Inbox"],
-      ["mdi-send", "Send"],
-      ["mdi-delete", "Trash"],
-      ["mdi-alert-octagon", "Spam"],
+      ["mdi-flag-variant-outline", "目標"],
+      ["mdi-folder-multiple-outline", "プロジェクト"],
+      ["mdi-help-circle-outline", "ガイド"],
+    ],
+    projects: [
+      ["mdi-folder-outline", "VizHD"],
+      ["mdi-folder-outline", "開発"],
+      ["mdi-folder-outline", "マーケティング"],
     ],
   }),
 };
