30d8238b3c98d8a583fa11cb1e4b089b4c29be83
shitogishi_yujin <hyerk.master@gmail.com>
2022-01-28 17:54:11 +0900

    メインコンテンツのサブタイトル

--- web/resources/js/pages/Project.vue
+++ web/resources/js/pages/Project.vue
@@ -4,6 +4,14 @@
     style="max-width: 900px"
     fluid
   >
+    <div class="d-flex justify-space-between">
+      <v-subheader class="pa-md-0" style="font-size: 1rem"
+        ><p class="ma-0 font-weight-bold" color="grey darken-1">
+          プロジェクト一覧
+        </p></v-subheader
+      >
+      <v-icon size="24">mdi-plus-circle</v-icon>
+    </div>
     <v-col v-for="card in cards" :key="card" :card="card" cols="12">
       <ProjectCards />
     </v-col>
