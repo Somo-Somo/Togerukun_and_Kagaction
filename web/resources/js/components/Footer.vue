72f2b2cbef8b56a3c9ff88cd62cf974c9916305e
shitogishi_yujin <hyerk.master@gmail.com>
2022-01-29 16:09:45 +0900

    フッター方面にaddボタン追加

--- web/resources/js/components/Footer.vue
+++ web/resources/js/components/Footer.vue
@@ -1,9 +1,25 @@
 <template>
-  <div class="d-flex d-sm-none">
+  <div class="d-flex flex-column d-md-none">
+    <!-- 追加ボタン -->
+    <div class="d-flex justify-center pa-6">
+      <v-avatar
+        size="80"
+        color="white"
+        style="position: relative; bottom: 0px; z-index: 10"
+      >
+        <v-avatar size="64" color="#03A9F4">
+          <v-icon size="32" color="white" style="backgroud-color: white">
+            mdi-pencil-plus
+          </v-icon>
+        </v-avatar>
+      </v-avatar>
+    </div>
+    <!-- フッターとナビゲーション -->
     <v-bottom-navigation
       v-model="value"
       class="d-flex justify-start"
       style="background-color: #80cbc4"
+      absolute
     >
       <v-bottom-sheet v-model="sheet">
         <template v-slot:activator="{ on, attrs }">
