9dca38e1c90c30eb44cb709bbd732c65fbd61633
shitogishi_yujin <hyerk.master@gmail.com>
2022-02-01 05:10:39 +0900

    dialogを切り分けることに成功

--- web/resources/js/components/AddCard.vue
+++ web/resources/js/components/AddCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center">
-    <v-dialog v-model="cardForm" width="500">
+    <v-dialog v-model="isDisplay" width="500">
       <template v-slot:activator="{ on, attrs }">
         <!-- PC版追加ボタン -->
         <NewAdditionalCard :on="on" :attrs="attrs" />
@@ -8,48 +8,26 @@
         <SpNewAdditionalBtn :on="on" :attrs="attrs" />
       </template>
       <!-- 追加のフォーム -->
-      <v-card ref="form">
-        <v-card-text class="d-flex align-content-center px-9 pt-9 pb-0">
-          <v-text-field
-            class="pa-0 ma-0"
-            v-model="message1"
-            label="プロジェクト名"
-            clearable
-          ></v-text-field>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn @click="cardForm = false" text> Cancel </v-btn>
-          <v-spacer></v-spacer>
-          <v-slide-x-reverse-transition> </v-slide-x-reverse-transition>
-          <v-btn color="primary" @click="cardForm = false" text> Next </v-btn>
-        </v-card-actions>
-      </v-card>
+      <ProjectNameInput :isDisplay="isDisplay"/>
     </v-dialog>
   </div>
 </template>
 
 <script>
 import NewAdditionalCard from "./Cards/NewAddtionalCard.vue";
-import NewAdditionalCard from "./Buttons/SpNewAdditionalBtn.vue";
-import SpNewAdditionalBtn from "./SpNewAdditionalBtn.vue";
+import SpNewAdditionalBtn from "./Buttons/SpNewAdditionalBtn.vue";
+import ProjectNameInput from "./Forms/ProjectNameInput.vue";
 
 export default {
   components: {
     NewAdditionalCard,
     SpNewAdditionalBtn,
-    SpNewAdditionalBtn,
+    ProjectNameInput
   },
-  data() {
-    return {
+  data: () => ({
       on: true,
       attrs: true,
-    };
-  },
-  props: {
-    cardForm: {
-      type: Boolean,
-    },
-  },
+      isDisplay: false
+  }),
 };
 </script>
---
+++ web/resources/js/components/Forms/ProjectNameInput.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-card ref="form">
+        <v-card-text class="d-flex align-content-center px-9 pt-9 pb-0">
+          <v-text-field
+            class="pa-0 ma-0"
+            v-model="message1"
+            label="プロジェクト名"
+            clearable
+          ></v-text-field>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn @click="isDisplay = false" text> Cancel </v-btn>
+          <v-spacer></v-spacer>
+          <v-slide-x-reverse-transition> </v-slide-x-reverse-transition>
+          <v-btn color="primary" @click="isDisplay = false" text> Next </v-btn>
+        </v-card-actions>
+      </v-card>
+
+</template>
+
+
+<script>
+export default({
+      props: {
+    isDisplay: {
+        type: Boolean,
+    }
+  },
+})
+</script>
