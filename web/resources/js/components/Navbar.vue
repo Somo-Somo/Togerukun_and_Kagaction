5dfbfe463824212ee667127a9b65b8401f6adbf7
shitogishi_yujin <hyerk.master@gmail.com>
2022-02-08 23:14:50 +0900

    ナビゲーションボタンの存在感を薄く

--- web/resources/js/components/Navbar.vue
+++ web/resources/js/components/Navbar.vue
@@ -4,14 +4,23 @@
       <v-app-bar
         class="d-flex px-0 py-0 mt-2"
         color="#80CBC4"
-        style="height: 72px;"
+        style="height: 72px"
         elevation="0"
         absolute
       >
-      <div class="d-flex justify-space-between" style="width: 222px;">
-      <img :src="'/img/VizHD_logo_app.png'" alt="アプリロゴ">
-      <v-icon class="my-6" style="height: 24px;" @click="$emit('clickMenu')">mdi-chevron-double-left</v-icon>
-      </div>
+        <v-hover v-slot="{ hover }">
+          <div class="d-flex justify-space-between" style="width: 222px">
+            <img :src="'/img/VizHD_logo_app.png'" alt="アプリロゴ" />
+            <v-icon
+              class="my-6"
+              style="height: 24px"
+              :class="{ 'show-btn': hover }"
+              :color="transparent"
+              @click="$emit('clickMenu')"
+              >mdi-chevron-double-left</v-icon
+            >
+          </div>
+        </v-hover>
       </v-app-bar>
       <v-list class="pb-4" style="padding-top: 72px">
         <v-list-item
@@ -99,6 +108,12 @@ export default {
       "開発",
       "マーケティング",
     ],
+    transparent: 'rgba(128, 128, 128, 0.3)',
   }),
 };
 </script>
+
+<style scoped lang='sass'>
+.show-btn
+  color: rgba(128, 128, 128, 1) !important
+</style>
