cc5ef0851b4e4132ac5e7c93e47b217a123ce49f
shitogishi_yujin <hyerk.master@gmail.com>
2022-02-09 06:28:51 +0900

    headerの戻るボタンにtoをつけた・exactつけたらactiveならん

--- web/resources/js/components/Header.vue
+++ web/resources/js/components/Header.vue
@@ -12,12 +12,23 @@
       class="mt-3 hidden-sm-and-down"
     ></v-app-bar-nav-icon>
     <v-toolbar-title class="d-flex justify-start mt-3 px-0 ml-md-2">
-      <v-icon
-        class="px-2"
-        v-if="headerTitle === '仮説一覧' || headerTitle === '仮説詳細'"
-        >mdi-chevron-left</v-icon
+      <v-btn
+        class="d-flex align-self-center"
+        :to="headerTitle === '仮説一覧' ? '/project' : '/project/123'"
+        exact
+        small
+        icon
+        link
       >
-      <h1 class="px-2" style="font-size: 24px">{{ headerTitle }}</h1>
+        <v-icon
+          class="px-2"
+          v-if="headerTitle === '仮説一覧' || headerTitle === '仮説詳細'"
+          >mdi-chevron-left</v-icon
+        >
+      </v-btn>
+      <h1 class="px-2 d-flex align-self-center" style="font-size: 24px">
+        {{ headerTitle }}
+      </h1>
     </v-toolbar-title>
   </v-app-bar>
 </template> 
