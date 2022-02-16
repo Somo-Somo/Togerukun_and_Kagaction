72cc042dce3bbfddd12bcbbe84c5759eea29e0ed
shitogishi_yujin <hyerk.master@gmail.com>
2022-02-17 02:11:26 +0900

    api.php一旦作成

--- web/routes/api.php
+++ web/routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\HypothesisController;
+use App\Http\Controllers\CurrentGoalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// プロジェクト
+Route::get('/projects', [ProjectController::class, 'index']);
+Route::post('/projects', [ProjectController::class, 'store']);
+Route::put('/projects/{projectId}', [ProjectController::class, 'update']);
+Route::delete('/projects/{projectId}', [ProjectController::class, 'destroy']);
+
+// 仮説
+Route::get('/projects/:id', [HypothesisController::class, 'index']);
+Route::get('/hypothesis/:hypothesisId', [HypothesisController::class, 'show']);
+Route::post('/hypothesis/{hypothesisId}', [HypothesisController::class, 'store']);
+Route::put('/hypothesis/{hypothesisId}', [HypothesisController::class, 'update']);
+Route::delete('/hypothesis/{hypothesisId}', [HypothesisController::class, 'destroy']);
+
+// 現在の目標
+Route::get('/current_goals', [HypothesisController::class, 'index']);
+Route::post('/current_goal/{hypothesisId}', [HypothesisController::class, 'store']);
+Route::delete('/current_goal/{hypothesisId}', [HypothesisController::class, 'destroy']);
