588c7b3b4f3baa3a6d5b9106f90266190c4da3f7
shitogishi_yujin <hyerk.master@gmail.com>
2022-02-18 08:46:19 +0900

    vue側のログイん処理

--- web/resources/js/pages/Login.vue
+++ web/resources/js/pages/Login.vue
@@ -7,67 +7,69 @@
       max-width="640"
     >
       <v-card-title class="text-center pa-8">
-        <h4 class="fill-width">{{ accountInputForm }}</h4>
+        <h4 class="fill-width">{{ isLoginForm ? 'ログイン' : '会員登録' }}</h4>
       </v-card-title>
       <v-divider> </v-divider>
-      <div class="px-6 py-8">
-        <div style="max-width:344px" class="mx-auto">
-          <div class="pt-6">
-            <div>
-              <v-text-field
-                v-model="name"
-                v-if="accountInputForm === '会員登録'"
-                autofocus
-                dense
-                height="48px"
-                outlined
-                placeholder="ユーザ名"
-              ></v-text-field>
+      <form class="form" @submit="submitForm">
+        <div class="px-6 py-8">
+          <div style="max-width: 344px" class="mx-auto">
+            <div class="pt-6">
+              <div>
+                <v-text-field
+                  v-model="registerForm.name"
+                  v-if="!isLoginForm"
+                  autofocus
+                  dense
+                  height="48px"
+                  outlined
+                  placeholder="ユーザ名"
+                ></v-text-field>
 
-              <v-text-field
-                v-model="email"
-                autofocus
-                dense
-                height="48px"
-                outlined
-                placeholder="メールアドレス"
-              ></v-text-field>
+                <v-text-field
+                  :model="isLoginForm ? loginForm.email : registerForm.email"
+                  autofocus
+                  dense
+                  height="48px"
+                  outlined
+                  placeholder="メールアドレス"
+                ></v-text-field>
 
-              <v-text-field
-                v-model="password"
-                :append-icon="passwordShow ? 'mdi-eye' : 'mdi-eye-off'"
-                :type="passwordShow ? 'text' : 'password'"
-                dense
-                height="48px"
-                name="input-password"
-                outlined
-                placeholder="パスワード"
-                @click:append="passwordShow = !passwordShow"
-              ></v-text-field>
-            </div>
-            <div class="login-btn pb-8">
-              <v-btn
-                class="fill-width caption"
-                color="#FFCB00"
-                depressed
-                height="48px"
-                tile
-              >
-                {{ accountInputForm }}
-              </v-btn>
-            </div>
-            <v-divider></v-divider>
-            <div class="pt-8 pb-4" v-if="accountInputForm === '会員登録'">
-              <span>すでにアカウントをお持ちですか？</span>
-              <p @click="accountInputForm = 'ログイン'">ログインに移動</p>
-            </div>
-            <div class="pt-8 pb-4" v-if="accountInputForm === 'ログイン'">
-              <span>アカウントをお持ちでない方はこちらへ</span>
-              <p @click="accountInputForm = '会員登録'">会員登録に移動</p>
+                <v-text-field
+                  :model="isLoginForm ? loginForm.password : registerForm.password"
+                  :append-icon="passwordShow ? 'mdi-eye' : 'mdi-eye-off'"
+                  :type="passwordShow ? 'text' : 'password'"
+                  dense
+                  height="48px"
+                  name="input-password"
+                  outlined
+                  placeholder="パスワード"
+                  @click:append="passwordShow = !passwordShow"
+                ></v-text-field>
+              </div>
+              <div class="login-btn pb-8">
+                <v-btn
+                  class="fill-width caption"
+                  color="#FFCB00"
+                  depressed
+                  height="48px"
+                  tile
+                >
+                  {{ isLoginForm ? 'ログイン' : '会員登録'}}
+                </v-btn>
+              </div>
+              <v-divider></v-divider>
+              <div class="pt-8 pb-4" v-if="!isLoginForm">
+                <span>すでにアカウントをお持ちですか？</span>
+                <p @click="isLoginForm = true">ログインに移動</p>
+              </div>
+              <div class="pt-8 pb-4" v-if="isLoginForm">
+                <span>アカウントをお持ちでない方はこちらへ</span>
+                <p @click="isLoginForm = false">会員登録に移動</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </v-card>
   </div>
 </template>
@@ -75,11 +77,40 @@
 <script>
 export default {
   data: () => ({
-    accountInputForm: '会員登録',
-    name: null,
-    email: null,
-    password: null,
+    isLoginForm: true,
+    loginForm: {
+        email: '',
+        password: ''
+    },
+    registerForm: {
+        name: '',
+        email: '',
+        password: '',
+    },
     passwordShow: false,
   }),
+  methods: {
+    submitForm() {
+      this.isLoginForm ? this.login() : this.register();
+    },
+    async login () {
+      // authストアのloginアクションを呼び出す
+      await this.$store.dispatch('auth/login', this.loginForm)
+
+      if (this.apiStatus) {
+        // トップページに移動する
+        this.$router.push('/')
+      }
+    },
+    async register () {
+      // authストアのresigterアクションを呼び出す
+      await this.$store.dispatch('auth/register', this.registerForm)
+
+      if (this.apiStatus) {
+        // トップページに移動する
+        this.$router.push('/')
+      }
+    },
+  },
 };
 </script>
