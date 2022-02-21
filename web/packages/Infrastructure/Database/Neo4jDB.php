80a1680347a48af0645200bdffe567cec81d788d
shitogishi_yujin <hyerk.master@gmail.com>
2022-02-21 22:02:51 +0900

    Facadeで使用したNeo4j.phpにコメント追加

--- web/packages/Infrastructure/Database/Neo4jDB.php
+++ web/packages/Infrastructure/Database/Neo4jDB.php
@@ -5,6 +5,9 @@ namespace packages\Infrastructure\Database;
 use \Laudis\Neo4j\ClientBuilder;
 use \Laudis\Neo4j\Authentication\Authenticate;
 
+/**
+ * Neo4jのDBを呼び出す処理をFacadeを使って共通化
+ */
 class Neo4jDB {
 
     private $client;
