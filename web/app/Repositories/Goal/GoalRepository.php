044da34c9119852349929866c29afefe48bc4816
shitogishi_yujin <hyerk.master@gmail.com>
2022-02-28 09:10:41 +0900

    goalを保存するcypher文を書きました

--- web/app/Repositories/Goal/GoalRepository.php
+++ web/app/Repositories/Goal/GoalRepository.php
@@ -16,6 +16,30 @@ class GoalRepository implements GoalRepositoryInterface
 
     public function create($goal)
     {
-        return $goal;
+        $createdGoal = $this->client->run(
+            <<<'CYPHER'
+                MATCH (user:User { email : $user_email }), (project:Project { uuid: $parent_uuid})
+                CREATE (user)-[
+                            :CREATED{at:localdatetime({timezone: 'Asia/Tokyo'})}
+                        ]->(
+                           hypothesis:Hypothesis {
+                                name: $name,
+                                uuid: $uuid,
+                                status: null,
+                                limited: null,
+                        }) - [
+                            ::IS_THE_GOAL_OF {since:localdatetime({timezone: 'Asia/Tokyo'})}  
+                        ] -> (project)
+                RETURN project
+                CYPHER,
+                [
+                    'name' => $goal['name'], 
+                    'uuid' => $goal['uuid'], 
+                    'parent_uuid' => $goal['parent_uuid'], 
+                    'user_email' => $goal['created_by_user_email'], 
+                ]
+            );
+
+        return $createdGoal;
     }
 }
