a98c0ec12eecc0894cf087b5c26c1654a91fc41e
shitogishi_yujin <hyerk.master@gmail.com>
2022-03-07 10:21:56 +0900

    store側の実装

--- web/resources/js/store/navigation.js
+++ web/resources/js/store/navigation.js
@@ -1,17 +1,21 @@
 const state = {
-
+    opendNavigation: true,
 };
 
 const getters = {
-
+    openedNavigation: state => state.opendNavigation,
 };
 
 const mutations = {
-
+    changeNavState (state) {
+        state.opendNavigation = !state.opendNavigation;
+    }
 }
 
 const actions = {
-
+    changeNavState (context) {
+        context.commit('changeNavState');
+    }
 }
 
 export default {
