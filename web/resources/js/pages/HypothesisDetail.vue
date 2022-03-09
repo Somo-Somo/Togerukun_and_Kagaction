2228c1f6033a3e56ec64a2a195104fca718e8e60
shitogishi_yujin <hyerk.master@gmail.com>
2022-03-09 11:41:15 +0900

    vueのテンプレート側に今日の目標チェックボックスを配置

--- web/resources/js/pages/HypothesisDetail.vue
+++ web/resources/js/pages/HypothesisDetail.vue
@@ -79,6 +79,27 @@
               </v-btn>
             </v-col>
           </div>
+          <div
+            class="py-2 d-flex justify-start"
+            :style="
+              $vuetify.breakpoint.mdAndUp ? 'height: 80px' : 'height: 64px'
+            "
+          >
+            <v-subheader
+              class="d-flex align-self-center pa-md-0"
+              :class="
+                $vuetify.breakpoint.mdAndUp
+                  ? 'hypothesisSubTitle'
+                  : 'spHypothesisSubTitle'
+              "
+            >
+              <p class="ma-0 font-weight-bold" color="grey darken-1">今日の目標:</p>
+            </v-subheader>
+            <v-col class="px-4 px-md-6 d-flex align-self-center">
+              <v-checkbox
+              ></v-checkbox>
+            </v-col>
+          </div>
           <div class="py-2">
             <div class="d-flex justify-space-between">
               <v-subheader
