72590ed0379a902cc431e1e1de71cb0dd3ac7bf0
shitogishi_yujin <hyerk.master@gmail.com>
2022-03-09 12:35:34 +0900

    今日の目標関連のlaravelの側だけ作成

--- web/app/Http/Controllers/TodaysGoalController.php
+++ web/app/Http/Controllers/TodaysGoalController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CurrentGoalController extends Controller
+class TodaysGoalController extends Controller
 {
     /**
      * Display a listing of the resource.
