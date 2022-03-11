826ae2bd5ac558f0a2efd1880f7ebec92b59fbef
shitogishi_yujin <hyerk.master@gmail.com>
2022-03-11 14:32:36 +0900

    初期化の時のエラー対応

--- web/resources/js/store/initialize.js
+++ web/resources/js/store/initialize.js
@@ -16,26 +16,28 @@ const mutations = {
 }
 
 const actions = {
-    getUserHasProjectAndHypothesis (context, route) {
-        axios.get('/api/initialize')
-            .then(response => {
-                console.info('プロジェクトと仮説を取得しました');                
-                console.info(response.data);                
-                context.commit ('project/setProjectList', response.data.project, { root: true });
-                context.commit ('hypothesis/setAllHypothesisList', response.data.hypothesis, { root: true });
-                context.commit ('finishedLoading');
-                if (route.name === "hypothesisList") {
-                    const projectUuid = route.params.id
-                    context.commit ('project/setProject', response.data.project[projectUuid] , { root: true });
-                    context.commit ('hypothesis/setHypothesisList', projectUuid , { root: true });
-                } else if (route.name === "hypothesisDetail") {
-                    router.push({ path: '/projects' });
-                }            
-                return false;
-            })
-            .catch(error => {
-                console.info(error);
-            });
+    async getUserHasProjectAndHypothesis (context, route) {
+        const response = await axios.get('/api/initialize')
+                .catch(err => {
+                    err.response || err
+                });
+
+        if (response.status === OK) {
+            context.commit ('project/setProjectList', response.data.project, { root: true });
+            context.commit ('hypothesis/setAllHypothesisList', response.data.hypothesis, { root: true });
+            context.commit ('finishedLoading');
+            if (route.name === "hypothesisList") {
+                const projectUuid = route.params.id
+                context.commit ('project/setProject', response.data.project[projectUuid] , { root: true });
+                context.commit ('hypothesis/setHypothesisList', projectUuid , { root: true });
+            } else if (route.name === "hypothesisDetail") {
+                router.push({ path: '/projects' });
+            }            
+            return false;
+        }
+
+        context.commit('setApiStatus', false);
+        context.commit('error/setCode', response.status, { root: true })
     },
 }
 
