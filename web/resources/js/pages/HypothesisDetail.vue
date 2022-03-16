e3f161b1d90303fdb054267fefa38e9b0315414c
shitogishi_yujin <hyerk.master@gmail.com>
2022-03-16 14:30:37 +0900

    仮設詳細で仮説を作った時ページ遷移しない挙動

--- web/resources/js/pages/HypothesisDetail.vue
+++ web/resources/js/pages/HypothesisDetail.vue
@@ -225,12 +225,7 @@ export default {
         name : this.inputFormName,
         parent_uuid: this.hypothesis.uuid,
       }
-      const createdHypothesis = await this.$store.dispatch("hypothesis/createHypothesis", hypothesis);
-      // ゴール作成後の遷移先
-      const url = "/hypothesis/" + createdHypothesis.hypothesis.uuid;
-      if (this.apiStatus) {
-        this.$router.push(url);
-      }
+      await this.$store.dispatch("hypothesis/createHypothesis", hypothesis);
     },
     edit(){
         this.$store.dispatch("hypothesis/editHypothesis", this.hypothesis);
