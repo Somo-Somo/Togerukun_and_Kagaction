fde20d7486cb182612642d44c14e9e3852bcf247
shitogishi_yujin <hyerk.master@gmail.com>
2022-03-20 21:24:17 +0900

    toggleを付けました

--- web/resources/js/components/Cards/HypothesisCard.vue
+++ web/resources/js/components/Cards/HypothesisCard.vue
@@ -5,9 +5,25 @@
       class="px-md-0"
       v-for="hypothesis in hypothesisList" 
       :key="hypothesis.uuid"
+      v-model="hypothesis.showHypothesisList"
       :class="cardShow(hypothesis) ? '' : 'd-none'"
       >
-      <v-card class="rounded" outlined>
+      <div class="d-flex">
+        
+      <div 
+        v-if="hypothesisStatus.name === '仮説一覧'"
+        class="d-flex">
+        <div v-if="hypothesis.noChild" style="width: 24px"></div>
+        <div v-if="!hypothesis.noChild" class="d-flex align-content-center"> 
+          <v-icon
+            v-if="hypothesis.toggle"
+            @click="onClickShowAndHideHypothesis(hypothesis)"
+            style="background-color: none;"
+            >{{ hypothesis.toggle }}
+          </v-icon>
+        </div>
+      </div>
+      <v-card class="rounded" style="width: 100%;" outlined>
         <v-list class="py-0 d-flex align-content-center" style="height: 80px">
           <v-list-item @click="toHypothesisDetail(hypothesis)" link>
             <v-list-item-content class="pa-0 d-flex flex-nowrap">
@@ -68,8 +84,9 @@
           </v-list-item>
         </v-list>
       </v-card>
+      </div>
     </v-col>
-    <div class="my-4" v-show="!hypothesisStatus.existsCard && hypothesisStatus.name !== 'ゴール'">
+    <div class="my-4" v-show="!hypothesisStatus.show && hypothesisStatus.name !== 'ゴール'">
       <p class="grey--text font-weight-bold ma-0 pa-2">
           {{hypothesisStatus.name}}はありません
       </p>
@@ -117,17 +134,25 @@ export default {
   },
   computed : {
     cardShow() {
-      return function (hypothesis) {
-        if (this.hypothesisStatus.name === "仮説一覧") 
-          return this.existsCard();
+      return function (hypothesis) {        
         if (this.hypothesisStatus.name === "ゴール") 
-          return hypothesis.depth === 1 ? this.existsCard() : false;
+          return hypothesis.depth === 1 ? this.showHypothesis() : false;
+        
+        if (this.hypothesisStatus.name === "仮説一覧") {
+          if(hypothesis) this.hypothesisStatus.show = true;
+          if (hypothesis.depth === 1) hypothesis.showHypothesisList = true;
+          return hypothesis.showHypothesisList ? true : false;
+        }
+
         if (this.hypothesisStatus.name === "今日の目標") 
-          return hypothesis.todaysGoal ? this.existsCard() : false;  
+          return hypothesis.todaysGoal ? this.showHypothesis() : false; 
+
         if (this.hypothesisStatus.name === "完了") 
-          return hypothesis.status ? this.existsCard() : false; 
+          return hypothesis.status ? this.showHypothesis() : false; 
+
         if (this.hypothesisStatus.name === "仮説詳細") 
-          return this.selectHypothesis.uuid === hypothesis.parentUuid ? this.existsCard() : false;
+          return this.selectHypothesis.uuid === hypothesis.parentUuid ? this.showHypothesis() : false;
+
         return false;
       }
     },
@@ -160,8 +185,8 @@ export default {
     }, 
   },
   methods: {
-    existsCard(){
-      this.hypothesisStatus.existsCard = true;
+    showHypothesis(){
+      this.hypothesisStatus.show = true;
       return true;
     },
     async toHypothesisDetail (hypothesis) {
@@ -189,7 +214,41 @@ export default {
       this.deletingConfirmationDialog = false;
       this.selectedDeletingHypothesis.name = null;
       this.selectedDeletingHypothesis.uuid = null;
-    }    
+    },
+    onClickShowAndHideHypothesis(hypothesis){
+      for (const hypothesisKey in this.hypothesisList) {
+        let key = Number(hypothesisKey);
+       if (hypothesis.uuid === this.hypothesisList[hypothesisKey].parentUuid){     
+          if(hypothesis.toggle === 'mdi-menu-right') {
+            this.hypothesisList[hypothesisKey].showHypothesisList = true
+          } else if (hypothesis.toggle === 'mdi-menu-down') {
+            this.hypothesisList[hypothesisKey].showHypothesisList = false;
+            while (key < Object.keys(this.hypothesisList).length) {
+              if(this.hypothesisList[hypothesisKey].depth  >  this.hypothesisList[key].depth){
+                break;
+              }
+              this.hypothesisList[key].showHypothesisList = false;
+              if(this.hypothesisList[key].toggle === 'mdi-menu-down') 
+                this.hypothesisList[key].toggle = 'mdi-menu-right';
+              
+              key = key + 1;
+            }
+          } 
+       }
+      }
+      if (hypothesis.toggle === 'mdi-menu-right') {
+        hypothesis.toggle = 'mdi-menu-down';
+      } else if (hypothesis.toggle === 'mdi-menu-down') {
+        hypothesis.toggle = 'mdi-menu-right';
+      }
+      return this.cardShow(hypothesis);
+    }
   },
 };
 </script>
+<style scoped lang='sass'>
+.toggleTransrateRight
+  transform: rotate(0.25turn)
+.v-icon.v-icon:after
+  background-color: transparent !important
+</style>
