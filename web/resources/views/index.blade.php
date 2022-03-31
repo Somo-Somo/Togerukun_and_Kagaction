84410e8d63e3d2b5b0fb09877cd8576af9039ec7
shitogishi_yujin <hyerk.master@gmail.com>
2022-03-31 21:23:38 +0900

    faviconの設定

--- web/resources/views/index.blade.php
+++ web/resources/views/index.blade.php
@@ -12,7 +12,10 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather|Roboto:400">
   <link rel="stylesheet" href="https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css">
 
-  <!-- Styles -->
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="{{ asset('favicon.ico')}}" />
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/kagaction-apple-touch-icon.png')}}">
+
 <style>
     .v-application {
         font-family: 
