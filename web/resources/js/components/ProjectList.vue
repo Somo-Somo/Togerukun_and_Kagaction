2d9d64dfc6bf7025efe8b2e3d4aeae783b5ee863
shitogishi_yujin <hyerk.master@gmail.com>
2022-04-20 18:56:04 +0900

    ホバーで3点リーダーを出すように

--- web/resources/js/components/ProjectList.vue
+++ web/resources/js/components/ProjectList.vue
@@ -20,9 +20,11 @@
     ></v-progress-circular>
     <v-list class="overflow-y-auto py-0" height="calc(100% - 304px)">
         <v-list-item
-            v-for="project in projectList"
-            :key="project.uuid"
+            v-for="(project, index) in projectList"
+            :key="index"
             @click="selectProject(project)"
+            @mouseover="showMenu = index;"
+            @mouseout="showMenu = false;"
             class="d-flex px-8"
             style="height: 48px"
             link
@@ -33,6 +35,9 @@
             <v-list-item-content class="align-self-center">
                 <v-list-item-title>{{ project.name }}</v-list-item-title>
             </v-list-item-content>
+            <v-list-item-icon class="align-self-center mx-0" v-show="showMenu === index">
+                <Menu :menus="cardMenus" :selectCard="project" @selectedMenu="selectedMenu"/>
+            </v-list-item-icon>
         </v-list-item>
         <v-list-item @click="onClickCreate" class="d-flex px-8" style="height: 48px" link>
             <v-list-item-icon class="align-self-center mr-6">
@@ -54,6 +59,13 @@
             :loading="submitLoading"
         />
     </form>
+    <DeletingConfirmationDialog 
+      :deletingConfirmationDialog="deletingConfirmationDialog"
+      :selectedDeletingItem="selectedDeletingProject"
+      :loading="submitLoading"
+      @deleteItem="deleteProject"
+      @onClickCancel="onClickCancel"
+    />
   </div>
 </template>
 
@@ -72,6 +84,16 @@ export default {
     data: () => ({
         category: "プロジェクト",
         inputFormCard: false,
+        deletingConfirmationDialog: false,
+        selectedDeletingProject: {
+            name: null,
+            uuid: null,
+        },
+        showMenu: false,
+        cardMenus: [
+            {title: "編集", color:"color: black"},
+            {title: "削除", color:"color: red"},
+        ],
         submitLoading: false,
     }),
     computed: {
@@ -101,10 +123,30 @@ export default {
         onClickCancel() {
             this.$store.dispatch("form/closeForm");
             this.inputFormCard = false;
+            this.deletingConfirmationDialog = false;
+            this.selectedDeletingProject.name = null;
+            this.selectedDeletingProject.uuid = null;
         },
         selectProject(project) {
             this.$store.dispatch("project/selectProject", project);
             return this.$router.push({ path: "/project/" + project.uuid });
+        },
+        selectedMenu(menuTitle, project){
+            if (menuTitle === "編集") {
+                this.onClickEdit(project);
+            } else if (menuTitle === "削除") {
+                this.deletingConfirmationDialog = true;
+                this.selectedDeletingProject.name = project.name;
+                this.selectedDeletingProject.uuid = project.uuid;
+            }
+        },
+        async deleteProject(){
+            this.submitLoading = true;
+            await this.$store.dispatch("project/deleteProject", this.selectedDeletingProject);
+            this.submitLoading = false;
+            this.deletingConfirmationDialog = false;
+            this.selectedDeletingProject.name = null;
+            this.selectedDeletingProject.uuid = null;
         },
         async submitForm() {
             if (this.submitType === "create") {
@@ -131,6 +173,9 @@ export default {
         inputForm(inputForm) {
             if (!inputForm) {
                 this.inputFormCard = false;
+                this.deletingConfirmationDialog = false;
+                this.selectedDeletingProject.name = null;
+                this.selectedDeletingProject.uuid = null;
             }
         },
     },
