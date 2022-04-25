8e28f57cf037f9677dc09274123b3dce893c68a3
shitogishi_yujin <hyerk.master@gmail.com>
2022-04-25 14:20:14 +0900

    全てを残り何日表示で

--- web/resources/js/components/Cards/HypothesisCard.vue
+++ web/resources/js/components/Cards/HypothesisCard.vue
@@ -296,7 +296,7 @@ export default {
         const today = (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10);
         const diff = (new Date(hypothesis.date) - new Date(today)) / (60*60*1000*24);
         if (diff > 0) {
-          this.subtitle.date.title = diff < 8 ? '残り' + diff + '日' : hypothesis.date;
+          this.subtitle.date.title = '残り' + diff + '日';
           this.subtitle.date.backgroundColor = diff < 4 ? 'background-color: yellow' : null;
         } else if (diff === 0) {
           this.subtitle.date.title = '今日';
