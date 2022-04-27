7f97535f0ec2469df9c640e2bd8d9d43d9c9e11f
shitogishi_yujin <hyerk.master@gmail.com>
2022-04-27 11:58:42 +0900

    UseCaseの作成

---
+++ web/app/UseCases/Comment/StoreAction.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\UseCases\Comment;
+
+use App\Repositories\Comment\CommentRepositoryInterface;
+
+class StoreAction
+{
+    protected $comment_repository;
+
+    public function __construct(CommentRepositoryInterface $commentRepositoryInterface)
+    {
+        $this->comment_repository = $commentRepositoryInterface;
+    }
+
+    public function invoke(array $comment)
+    {
+
+        $this->comment_repository->storeComment($comment);
+        
+    }
+}
