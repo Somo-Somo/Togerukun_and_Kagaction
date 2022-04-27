538e4eda6da3b51e155d2686c80090719d28c21b
shitogishi_yujin <hyerk.master@gmail.com>
2022-04-27 12:23:37 +0900

    Controller側の処理

--- web/app/Http/Controllers/CommentController.php
+++ web/app/Http/Controllers/CommentController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\UseCases\Comment\StoreAction;
+use App\UseCases\Comment\UpdateAction;
+use App\UseCases\Comment\DestroyAction;
+use App\Http\Requests\CommentRequest;
 use Illuminate\Http\Request;
+use \Symfony\Component\HttpFoundation\Response;
 
 class CommentController extends Controller
 {
@@ -19,12 +24,27 @@ class CommentController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $hypothesisUuid
+     * @param  \Illuminate\Http\CommentRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(string $hypothesisUuid, CommentRequest $request, StoreAction $storeAction)
     {
-        //
+        $comment = [
+            'user_email' => $request->user()->email,
+            'hypothesis_uuid' => $hypothesisUuid,
+            'comment_uuid' => $request->uuid,
+            'text' => $request->text
+        ];
+
+        $storeAction->invoke($comment);
+
+        $json = [
+            'message' => 'コメントを追加しました',
+            'error' => '',
+        ];
+
+        return response()->json($json, Response::HTTP_CREATED);
     }
 
     /**
@@ -41,13 +61,25 @@ class CommentController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\CommentRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(CommentRequest $request, UpdateAction $updateAction)
     {
-        //
+        $comment = [
+            'user_email' => $request->user()->email,
+            'uuid' => $request->uuid,
+            'text' => $request->text
+        ];
+
+        $updateAction->invoke($comment);
+
+        $json = [
+            'message' => 'コメントを更新しました',
+            'error' => '',
+        ];
+
+        return response()->json($json, Response::HTTP_OK);
     }
 
     /**
@@ -56,8 +88,20 @@ class CommentController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request, DestroyAction $destroyAction)
     {
-        //
+        $comment = [
+            'user_email' => $request->user()->email,
+            'uuid' => $request->uuid
+        ];
+
+        $destroyAction->invoke($comment);
+
+        $json = [
+            'message' => 'コメントを削除しました',
+            'error' => '',
+        ];
+
+        return response()->json($json, Response::HTTP_OK);
     }
 }
