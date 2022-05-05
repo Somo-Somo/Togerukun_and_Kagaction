66ad2b33958a4101727135b7d88b03469f82ad78
shitogishi_yujin <hyerk.master@gmail.com>
2022-05-05 18:13:23 +0900

    削除ボタンの追加

--- web/resources/js/components/Comments.vue
+++ web/resources/js/components/Comments.vue
@@ -1,71 +1,97 @@
 <template>
     <v-list>
-        <v-col
+        <v-list-item
             class="d-flex px-1 py-2"
-            v-for="comment in hypothesis.comments"
-            :key="comment.title"
+            v-for="(comment, index) in hypothesis.comments"
+            @mouseover="commentIndex = index"
+            @mouseout="commentIndex = false"
+            :key="index"
+            link
         >
-            <v-list-item-avatar class="d-flex align-self-center my-auto mx-0">
-                <v-avatar
-                    class="ma-1"
-                    color="brown"
-                    :size="$vuetify.breakpoint.smAndUp ? '30' : '28'"
+            <div class="d-flex">
+                <v-list-item-avatar
+                    class="d-flex align-self-center my-auto mx-0"
                 >
-                    <span
-                        class="white--text 'text-h6"
-                        :style="'font-size: 0.8rem !important;'"
+                    <v-avatar
+                        class="ma-1"
+                        color="brown"
+                        :size="$vuetify.breakpoint.smAndUp ? '30' : '28'"
                     >
-                        {{ getInitial(comment) }}
-                    </span>
-                </v-avatar>
-            </v-list-item-avatar>
-            <div class="d-flex flex-column px-2">
-                <div class="d-flex">
-                    <div class="d-flex" style="height: 24px">
-                        <p
-                            class="d-flex align-self-center font-weight-bold caption ma-0"
-                            style="font-size: 0.65rem !important"
+                        <span
+                            class="white--text 'text-h6"
+                            :style="'font-size: 0.8rem !important;'"
                         >
-                            {{ comment.user_name }}
-                        </p>
-                    </div>
-                    <div>
-                        <v-subheader class="px-2" style="height: 24px">
+                            {{ getInitial(comment) }}
+                        </span>
+                    </v-avatar>
+                </v-list-item-avatar>
+                <div class="d-flex flex-column px-2">
+                    <div class="d-flex">
+                        <div class="d-flex" style="height: 24px">
                             <p
                                 class="d-flex align-self-center font-weight-bold caption ma-0"
-                                style="font-size: 0.5rem !important"
+                                style="font-size: 0.65rem !important"
                             >
-                                {{ getDateAndTime(comment) }}
+                                {{ comment.user_name }}
                             </p>
-                        </v-subheader>
+                        </div>
+                        <div>
+                            <v-subheader class="px-2" style="height: 24px">
+                                <p
+                                    class="d-flex align-self-center font-weight-bold caption ma-0"
+                                    style="font-size: 0.5rem !important"
+                                >
+                                    {{ getDateAndTime(comment) }}
+                                </p>
+                            </v-subheader>
+                        </div>
+                    </div>
+                    <div>
+                        <p
+                            class="d-flex align-self-center font-weight-bold caption ma-0"
+                            style="
+                                color: #424242;
+                                font-size: 0.95rem !important;
+                            "
+                        >
+                            {{ comment.text }}
+                        </p>
                     </div>
                 </div>
-                <div>
-                    <p
-                        class="d-flex align-self-center font-weight-bold caption ma-0"
-                        style="color: #424242; font-size: 0.95rem !important"
-                    >
-                        {{ comment.text }}
-                    </p>
-                </div>
             </div>
-        </v-col>
+            <div class="px-1" style="position:absolute; right:8px;" v-show="commentIndex === index">
+                <v-list-item-icon
+                    class="d-flex align-self-center ma-0"
+                >
+                    <Menu
+                        :menus="menus"
+                        :selectCard="comment"
+                        @selectedMenu="selectedMenu"
+                    />
+                </v-list-item-icon>
+            </div>
+        </v-list-item>
     </v-list>
 </template>
 
 <script>
+import Menu from "../components/Buttons/Menu.vue";
 export default {
-    data: () => ({}),
+    components: {
+        Menu,
+    },
+    data: () => ({
+        menus: [{ title: "削除", color: "color: red" }],
+        commentIndex: false,
+    }),
     props: {
         hypothesis: {
             type: Object,
         },
     },
-    computed: {
-
-    },
+    computed: {},
     methods: {
-        getInitial(comment){
+        getInitial(comment) {
             return comment.user_name.charAt(0);
         },
         getDateAndTime(comment) {
@@ -74,7 +100,10 @@ export default {
             const date = dateAndTime.getDate();
             const hours = dateAndTime.getHours();
             const minutes = dateAndTime.getMinutes();
-            return month + '月' + date + '日 ' + hours + ':' + minutes;
+            return month + "月" + date + "日 " + hours + ":" + minutes;
+        },
+        selectedMenu(){
+            return;
         }
     },
 };
