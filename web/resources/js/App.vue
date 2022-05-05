4692ceed2efa5895fad079d0c493cbb6e1a64a3e
shitogishi_yujin <hyerk.master@gmail.com>
2022-05-06 08:35:13 +0900

    loginページにいる時はナビゲーションオフのしよう

--- web/resources/js/App.vue
+++ web/resources/js/App.vue
@@ -1,12 +1,12 @@
 <template>
   <v-app id="inspire">
-    <Navbar v-if="!errorCode" />
+    <Navbar v-if="checkPath"/>
     <v-main 
       class="my-md-2" 
       :class="$vuetify.breakpoint.mdAndUp ? '' : 'py-0'">
       <RouterView />
     </v-main>
-    <Footer v-if="user" />
+    <Footer v-if="checkPath" />
   </v-app>
 </template>
 
@@ -25,8 +25,12 @@ export default {
     ...mapGetters({
       check: 'auth/check',
       user: 'auth/user',
+      apiStatus: 'auth/apiStatus',
       errorCode: 'error/code',
     }),
+    checkPath(){
+      return this.$route.path !== '/login' ? true : false;
+    }
   },
   watch: {
     errorCode(val, old) {
@@ -39,7 +43,7 @@ export default {
           this.$router.push('/not-found');
         }
     },
-    $route() {
+    $route(to, from) {
       this.$store.commit("error/setCode", null);
     },
   },
