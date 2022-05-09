0859b2d44a50b9da2851a6c5373a1e257ac50e92
shitogishi_yujin <hyerk.master@gmail.com>
2022-05-09 17:31:53 +0900

    styleを整える

--- web/resources/js/components/Table.vue
+++ web/resources/js/components/Table.vue
@@ -1,44 +1,141 @@
 <template>
-    <v-card class="ma-4" color="#26c6da">
-        <v-list class="py-0">
-            <v-list-item-group color="primary">
-                <v-list-item
-                    v-for="(item, i) in items"
-                    :key="i"
-                    style="height: 88px"
+    <v-card class="ma-4">
+        <v-list-item-group color="primary">
+            <v-list-item
+                class="px-0"
+                :style="
+                    $vuetify.breakpoint.smAndUp ? 'height:88px' : 'height:64px'
+                "
+                v-for="(todo, i) in todos"
+                :key="i"
+                link
+            >
+                <div class="d-flex">
+                    <DashedLine />
+                </div>
+                <div
+                    class="d-flex ma-auto"
+                    style="width: 36px; height: 80px"
                 >
-                    <span>
-                        <DashedLine />
-                    </span>
-                    <span>
-                        <TDashedLine />
-                    </span>
-                    <div>
-                        <div style="height: 28px">
-                            <UpperOrLowerDashedLine />
-                        </div>
-                        <div style="height: 24px">
-                            <v-btn icon height="24">
-                                <v-icon
-                                    >mdi-checkbox-marked-circle-outline</v-icon
-                                >
-                            </v-btn>
-                        </div>
-                        <div style="height: 28px">
-                            <UpperOrLowerDashedLine />
-                        </div>
+                    <div class="d-flex align-content-center ma-auto" style="height: 24px">
+                        <v-btn icon height="24" width="24">
+                            <v-icon>mdi-checkbox-marked-circle-outline</v-icon>
+                        </v-btn>
                     </div>
-                    <span>
-                        <LDashedLine />
-                    </span>
-                    <v-list-item-content>
-                        <v-list-item-title
-                            v-text="item.text"
-                        ></v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list-item-group>
-        </v-list>
+                </div>
+                <v-list-item-content class="px-2 py-0 ma-auto d-flex">
+                    <div>
+                        <v-list-item-subtitle
+                            class="d-flex align-content-start mt-3 mb-1"
+                        >
+                            <div
+                                class="d-flex"
+                                :style="subTitle().backgroundColor"
+                                v-if="subTitle()"
+                            >
+                                <v-icon
+                                    :size="subTitle().iconSize"
+                                    :color="subTitle().iconColor"
+                                    >{{ subTitle().icon }}</v-icon
+                                >
+                                <p
+                                    class="ma-0 px-2 font-weight-bold align-self-center"
+                                    :class="subTitle().fontColor"
+                                    :style="
+                                        $vuetify.breakpoint.smAndUp
+                                            ? 'font-size:12px'
+                                            : 'font-size:8px'
+                                    "
+                                >
+                                    {{ subTitle().title }}
+                                </p>
+                            </div>
+                            <div class="d-flex" style="max-width: 66%">
+                                <p
+                                    class="ma-0 grey--text font-weight-bold align-self-center"
+                                    style="
+                                        font-size: 8px;
+                                        max-width: 100%;
+                                        white-space: nowrap;
+                                        overflow: hidden;
+                                        text-overflow: ellipsis;
+                                    "
+                                >
+                                    「お腹すいた
+                                </p>
+                                <p
+                                    class="ma-0 grey--text font-weight-bold align-self-center"
+                                    style="font-size: 8px"
+                                >
+                                    」のためのToDo
+                                </p>
+                            </div>
+                        </v-list-item-subtitle>
+                        <v-list-item-title class="py-2 pb-4">
+                            <p
+                                class="font-weight-black ma-0"
+                                style="
+                                    max-width: calc(100% - 36px);
+                                    white-space: nowrap;
+                                    overflow: hidden;
+                                    text-overflow: ellipsis;
+                                "
+                                :style="
+                                    $vuetify.breakpoint.smAndUp
+                                        ? 'font-size:1rem'
+                                        : 'font-size:0.8rem'
+                                "
+                            >
+                                {{ todo.name }}
+                            </p></v-list-item-title
+                        >
+                        <v-menu class="rounded-lg elevation-0" offset-y>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-list-item-action
+                                    class="ma-0"
+                                    style="position: absolute; right: 16px"
+                                    :style="
+                                        $vuetify.breakpoint.smAndUp
+                                            ? 'top: 28px;'
+                                            : 'top: 24px;'
+                                    "
+                                >
+                                    <v-btn
+                                        v-bind="attrs"
+                                        v-on="on"
+                                        small
+                                        icon
+                                        link
+                                    >
+                                        <v-icon
+                                            :size="
+                                                $vuetify.breakpoint.smAndUp
+                                                    ? '24'
+                                                    : '20'
+                                            "
+                                        >
+                                            mdi-dots-vertical
+                                        </v-icon>
+                                    </v-btn>
+                                </v-list-item-action>
+                            </template>
+                            <v-list>
+                                <v-list-item
+                                    v-for="menu in cardMenu"
+                                    :key="menu.title"
+                                    @click="selectMenu(menu.title, todo)"
+                                    link
+                                >
+                                    <v-list-item-title :style="menu.color">{{
+                                        menu.title
+                                    }}</v-list-item-title>
+                                </v-list-item>
+                            </v-list>
+                        </v-menu>
+                    </div>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list-item-group>
     </v-card>
 </template>
 
@@ -56,18 +153,42 @@ export default {
         UpperOrLowerDashedLine,
     },
     data: () => ({
-        items: [
-            { text: "テストで450点を取りたい", depth: "0" },
-            { text: "英語で90点をとる", depth: "1" },
-            { text: "英単語30個を勉強する", depth: "2" },
-            { text: "毎朝バスの中でやる", depth: "3" },
-            { text: "数学で90点をとる", depth: "1" },
-            { text: "数学ノートをやる", depth: "2" },
-            { text: "社会で90点を取る", depth: "1" },
+        todos: [
+            { name: "テストで450点を取りたい", depth: "0" },
+            { name: "英語で90点をとる", depth: "1" },
+            { name: "英単語30個を勉強する", depth: "2" },
+            { name: "毎朝バスの中でやる", depth: "3" },
+            { name: "数学で90点をとる", depth: "1" },
+            { name: "数学ノートをやる", depth: "2" },
+            { name: "社会で90点を取る", depth: "1" },
         ],
+        subtitle: {
+            accomplish: {
+                icon: "mdi-circle",
+                iconSize: 8,
+                title: "完了",
+                backgroundColor: "background-color: null",
+                iconColor: "green",
+                fontColor: "#212121--text",
+            },
+            date: {
+                icon: "mdi-clock-outline",
+                iconSize: 14,
+                iconColor: "#212121",
+                fontColor: "#212121--text",
+            },
+        },
+        cardMenu: [{ title: "削除", color: "color: red" }],
     }),
     props: {},
-    computed: {},
+    computed: {
+        subTitle() {
+            return () => {
+                this.subtitle.date.title = "残り10日";
+                return this.subtitle.date;
+            };
+        },
+    },
     methods: {},
 };
 </script>
