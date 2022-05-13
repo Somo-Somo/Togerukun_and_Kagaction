915993859f16d4ae650df738a8ee64663a6ac678
shitogishi_yujin <hyerk.master@gmail.com>
2022-05-13 22:38:27 +0900

    日付の処理など大幅リファクタ

--- web/resources/js/components/Table/TodoTableItemContent.vue
+++ web/resources/js/components/Table/TodoTableItemContent.vue
@@ -3,24 +3,24 @@
         <v-list-item-subtitle class="d-flex align-content-start mt-3 mb-1">
             <div
                 class="d-flex"
-                :style="subTitle(todo).backgroundColor"
-                v-if="subTitle(todo)"
+                :style="dateBackGroundColor(todo)"
+                v-if="todo.date"
             >
                 <v-icon
-                    :size="subTitle(todo).iconSize"
-                    :color="subTitle(todo).iconColor"
-                    >{{ subTitle(todo).icon }}</v-icon
+                    :size="date.iconSize"
+                    :color="date.iconColor"
+                    >{{ date.icon }}</v-icon
                 >
                 <p
                     class="ma-0 px-2 font-weight-bold align-self-center"
-                    :class="subTitle(todo).fontColor"
+                    :class="date.fontColor"
                     :style="
                         $vuetify.breakpoint.smAndUp
                             ? 'font-size:12px'
                             : 'font-size:8px'
                     "
                 >
-                    {{ subTitle(todo).title }}
+                    {{ dateTitle(todo) }}
                 </p>
             </div>
             <div class="d-flex" style="max-width: 66%">
@@ -68,14 +68,11 @@
 <script>
 export default {
     data: () => ({
-        todoStatus: {
-            date: {
-                icon: "mdi-clock-outline",
-                iconSize: 14,
-                iconColor: "#212121",
-                fontColor: "#212121--text",
-                backgroundColor: "background-color: transparent",
-            },
+        date: {
+            icon: "mdi-clock-outline",
+            iconSize: 14,
+            iconColor: "#212121",
+            fontColor: "#212121--text",
         },
         cardMenu: [{ title: "削除", color: "color: red" }],
     }),
@@ -91,10 +88,38 @@ export default {
         },
     },
     computed: {
-        subTitle() {
+        dateTitle() {
             return (todo) => {
-                return todo.date ? this.calcDate(todo) : false;
-            };
+                const diff = this.calcDateDiff(todo);
+                if (diff > 0) {
+                    return "残り" + diff + "日";
+                } else if (diff === 0) {
+                    return "今日";
+                } else if (todo.accomplish) {
+                    const year = new Date(todo.date).getFullYear();
+                    const month = new Date(todo.date).getMonth() + 1;
+                    const day = new Date(todo.date).getDay() + 1;
+                    return new Date().getFullYear() === year
+                            ? month + "月" + day + "日"
+                            : year + "年" + month + "月" + day + "日";
+                } else {
+                    return Math.abs(diff) + "日経過";
+                }
+            }
+        },
+        dateBackGroundColor(){
+            return (todo) => {
+                const diff = this.calcDateDiff(todo);
+                if (diff > 0) {
+                    return  diff < 4 ? "background-color: yellow" : "background-color: transparent";
+                } else if (diff === 0) {
+                    return "background-color: skyblue";
+                } else if (todo.accomplish) {
+                    return "background-color: transparent;";
+                } else {
+                    return "background-color: coral";
+                }
+            }
         },
         parentName() {
             return (todo) => {
@@ -118,7 +143,7 @@ export default {
         },
     },
     methods: {
-        calcDate(todo) {
+        calcDateDiff(todo) {
             const today = new Date(
                 Date.now() - new Date().getTimezoneOffset() * 60000
             )
@@ -126,31 +151,7 @@ export default {
                 .substr(0, 10);
             const diff =
                 (new Date(todo.date) - new Date(today)) / (60 * 60 * 1000 * 24);
-
-            if (diff > 0) {
-                this.todoStatus.date.title = "残り" + diff + "日";
-                this.todoStatus.date.backgroundColor =
-                    diff < 4 ? "background-color: yellow" : null;
-            } else if (diff === 0) {
-                this.todoStatus.date.title = "今日";
-                this.todoStatus.date.backgroundColor =
-                    "background-color: skyblue";
-            } else if (todo.accomplish) {
-                const year = new Date(todo.date).getFullYear();
-                const month = new Date(todo.date).getMonth() + 1;
-                const day = new Date(todo.date).getDay() + 1;
-                this.todoStatus.date.title  =
-                    new Date().getFullYear() === year
-                        ? month + "月" + day + "日"
-                        : year + "年" + month + "月" + day + "日";
-                this.todoStatus.date.backgroundColor =
-                    "background-color: transparent;";
-            } else {
-                this.todoStatus.date.title = Math.abs(diff) + "日経過";
-                this.todoStatus.date.backgroundColor =
-                    "background-color: coral";
-            }
-            return this.todoStatus.date;
+            return diff;
         },
     },
 };
