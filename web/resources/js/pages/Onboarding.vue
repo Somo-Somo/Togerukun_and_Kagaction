5bd43e21c8bc2ecb7604331ba2c2c73aea4234d4
shitogishi_yujin <hyerk.master@gmail.com>
2022-05-17 14:57:18 +0900

    titleの側

--- web/resources/js/pages/Onboarding.vue
+++ web/resources/js/pages/Onboarding.vue
@@ -29,17 +29,16 @@
 
                 <v-stepper-items>
                     <v-stepper-content step="1">
-                        <v-card
-                            class="mb-12"
-                            color="grey lighten-1"
-                            height="200px"
-                        ></v-card>
+                        <div>
+                            <p class="font-weight-black" style="font-size: 1.75rem;">〇〇さんが現在取り組んでいることまたはこれから取り組みたいことは何ですか？</p>
+                            <p class="subtitle-2 #757575--text" style="font-weight: 600!important;">仕事や普段の生活などで頑張りたいことや習慣にしたいこと、改善したいことでも大丈夫です。</p>
+                        </div>
 
                         <v-btn color="primary" @click="step = 2">
-                            Continue
+                            次へ
                         </v-btn>
 
-                        <v-btn text> Cancel </v-btn>
+                        <v-btn text> 戻る </v-btn>
                     </v-stepper-content>
 
                     <v-stepper-content step="2">
