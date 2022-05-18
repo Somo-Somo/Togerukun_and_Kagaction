e5f5f3bd28aea0e53e318806ff95209e786e1390
shitogishi_yujin <hyerk.master@gmail.com>
2022-05-19 02:08:37 +0900

    ローカルの方でうまくいくかやってみる

--- web/bootstrap/app.php
+++ web/bootstrap/app.php
@@ -20,7 +20,7 @@ switch ($_SERVER['HTTP_HOST'] ?? 'localhost:8080') {
         $app->loadEnvironmentFrom('.env.dev');
         break;
     case 'kagaction.herokuapp.com':
-        $app->loadEnvironmentFrom('.env.prod');
+        $app->loadEnvironmentFrom('.env.dev');
         break;
 }
 /*
