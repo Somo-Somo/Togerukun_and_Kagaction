aa6c1297d8cb4c53b27bd0f7480f035d6ac9edb7
shitogishi_yujin <hyerk.master@gmail.com>
2022-05-19 03:52:00 +0900

    passwordミスってた

--- web/resources/js/store/todo.js
+++ web/resources/js/store/todo.js
@@ -216,6 +216,7 @@ const actions = {
         const response = await axios.post('/api/todo', todo);
 
         if (response.status === UNPROCESSABLE_ENTITY) {
+            // console.info('エラー')
             // context.commit ('setRegisterErrorMessages', response.data.errors);
         } 
 
