92c63cef51619af248bda384ad8922e33208bf7c
shitogishi_yujin <hyerk.master@gmail.com>
2022-06-16 14:14:52 +0900

    削除したstore読み込むの消した

--- web/resources/js/store/index.js
+++ web/resources/js/store/index.js
@@ -1,30 +1,28 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
 
-import auth from './auth';
-import form from './form';
-import error from './error';
-import onboarding from './onboarding';
-import project from './project';
-import todo from './todo';
-import initialize from './initialize';
-import navigation from './navigation';
-import schedule from './schedule';
+import auth from "./auth";
+import form from "./form";
+import error from "./error";
+import onboarding from "./onboarding";
+import project from "./project";
+import todo from "./todo";
+import initialize from "./initialize";
+import navigation from "./navigation";
 
-Vue.use (Vuex);
+Vue.use(Vuex);
 
-const store = new Vuex.Store ({
-  modules: {
-    auth,
-    form,
-    error,
-    onboarding,
-    project,
-    todo,
-    initialize,
-    navigation,
-    schedule,
-  },
+const store = new Vuex.Store({
+    modules: {
+        auth,
+        form,
+        error,
+        onboarding,
+        project,
+        todo,
+        initialize,
+        navigation,
+    },
 });
 
 export default store;
