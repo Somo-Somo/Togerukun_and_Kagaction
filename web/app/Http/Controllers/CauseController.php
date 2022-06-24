58b21693e351a0b482247ff6717d0ebe078a7442
shitogishi_yujin <hyerk.master@gmail.com>
2022-06-24 15:57:15 +0900

    controller作成

--- web/app/Http/Controllers/CauseController.php
+++ web/app/Http/Controllers/CauseController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\UseCases\Cause\StoreAction;
+use App\UseCases\Cause\DestroyAction;
+use App\Http\Requests\CauseRequest;
+use \Symfony\Component\HttpFoundation\Response;
 
 class CauseController extends Controller
 {
@@ -29,12 +33,27 @@ class CauseController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $todoUuid
+     * @param  App\Http\Requests\CauseRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(string $todoUuid, CauseRequest $request, StoreAction $storeAction)
     {
-        //
+        $cause = [
+            'user_email' => $request->user()->email,
+            'todo_uuid' => $todoUuid,
+            'cause_uuid' => $request->uuid,
+            'text' => $request->text
+        ];
+
+        $storeAction->invoke($cause);
+
+        $json = [
+            'message' => '原因を追加しました',
+            'error' => '',
+        ];
+
+        return response()->json($json, Response::HTTP_CREATED);
     }
 
     /**
@@ -74,11 +93,24 @@ class CauseController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  string  $causeUuid
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(string $causeUuid, Request $request, DestroyAction $destroyAction)
     {
-        //
+        $cause = [
+            'user_email' => $request->user()->email,
+            'cause_uuid' => $causeUuid,
+        ];
+
+        $destroyAction->invoke($cause);
+
+        $json = [
+            'message' => '原因を削除しました',
+            'error' => '',
+        ];
+
+        return response()->json($json, Response::HTTP_OK);
     }
 }
