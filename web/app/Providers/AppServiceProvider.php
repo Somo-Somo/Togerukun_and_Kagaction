b7de81a3df3086e4361a0fd8609f38ed4e6c887c
shitogishi_yujin <hyerk.master@gmail.com>
2022-06-24 15:47:40 +0900

    リポジトリ~を使えるように

--- web/app/Providers/AppServiceProvider.php
+++ web/app/Providers/AppServiceProvider.php
@@ -13,27 +13,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind( 
+        $this->app->bind(
             \App\Repositories\Project\ProjectRepositoryInterface::class,
             \App\Repositories\Project\ProjectRepository::class,
         );
-        $this->app->bind( 
+        $this->app->bind(
             \App\Repositories\Date\DateRepositoryInterface::class,
             \App\Repositories\Date\DateRepository::class,
         );
-        $this->app->bind( 
+        $this->app->bind(
             \App\Repositories\Comment\CommentRepositoryInterface::class,
             \App\Repositories\Comment\CommentRepository::class,
         );
-        $this->app->bind( 
+        $this->app->bind(
+            \App\Repositories\Cause\CauseRepositoryInterface::class,
+            \App\Repositories\Cause\CauseRepository::class,
+        );
+        $this->app->bind(
             \App\Repositories\Todo\TodoRepositoryInterface::class,
             \App\Repositories\Todo\TodoRepository::class,
         );
-        $this->app->bind( 
+        $this->app->bind(
             \App\Repositories\Goal\GoalRepositoryInterface::class,
             \App\Repositories\Goal\GoalRepository::class,
         );
-        $this->app->bind( 
+        $this->app->bind(
             \App\Repositories\User\UserRepositoryInterface::class,
             \App\Repositories\User\UserRepository::class,
         );
