5aa519e19bb6ba46fce499ba75005d5dd605df8c
shitogishi_yujin <hyerk.master@gmail.com>
2022-06-29 18:44:33 +0900

    改変後の大枠

--- web/resources/js/components/InputForm.vue
+++ web/resources/js/components/InputForm.vue
@@ -1,79 +1,98 @@
 <template>
-<v-dialog v-model="inputForm" @click:outside="$emit('onClickCancel')" width="500">
-  <v-card ref="form">
-        <v-card-text class="d-flex align-content-center px-9 pt-9 pb-0">
-          <v-text-field
-            class="pa-0 ma-0"
-            v-model="formName"
-            counter="64"
-            :label="category"
-            @keypress.enter.prevent="$emit('submitForm')"
-            clearable
-          ></v-text-field>
-        </v-card-text>
-    <v-divider></v-divider>
-    <v-card-actions>
-      <v-btn @click="$emit('onClickCancel')" text>
-        キャンセル
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-slide-x-reverse-transition> </v-slide-x-reverse-transition>
-      <v-btn
-        type="submit"
-        :loading="loading"
-        :disabled="!formName || loading"
-        color="primary"
-        @click="$emit('submitForm')"
-        text
-      >
-        完了
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-</v-dialog>
+    <v-dialog
+        v-model="inputForm"
+        @click:outside="$emit('onClickCancel')"
+        width="500"
+    >
+        <v-card ref="form">
+            <v-card-text class="d-flex align-content-center px-9 pt-9 pb-0">
+                <v-text-field
+                    class="pa-0 ma-0"
+                    v-model="formName"
+                    counter="64"
+                    :label="category"
+                    @keypress.enter.prevent="$emit('submitForm')"
+                    clearable
+                ></v-text-field>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+                <v-btn v-if="cancel" @click="$emit('onClickCancel')" text>
+                    キャンセル
+                </v-btn>
+                <v-btn v-if="!cancel" @click="$emit('onClickCancel')" text>
+                    戻る
+                </v-btn>
+                <v-spacer></v-spacer>
+                <v-slide-x-reverse-transition> </v-slide-x-reverse-transition>
+                <v-btn
+                    v-if="done"
+                    type="submit"
+                    :loading="loading"
+                    :disabled="!formName || loading"
+                    color="primary"
+                    @click="$emit('submitForm')"
+                    text
+                >
+                    完了
+                </v-btn>
+                <v-btn
+                    v-if="!done"
+                    type="submit"
+                    :loading="loading"
+                    :disabled="!formName || loading"
+                    color="primary"
+                    @click="$emit('submitForm')"
+                    text
+                >
+                    次へ
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </template>
-
 
 <script>
 export default {
-  data: () => ({
-
-  }),
-  props: {
-    category: {
-      type: String,
+    data: () => ({
+        text: null,
+        date: null,
+        dateForm: false,
+        cancel: true,
+        done: true,
+    }),
+    props: {
+        category: {
+            type: String,
+        },
+        inputForm: {
+            type: Boolean,
+        },
+        hypotheses: {
+            type: Object,
+        },
+        loading: {
+            type: Boolean,
+        },
     },
-    inputForm: {
-      type: Boolean,
+    computed: {
+        formName: {
+            get() {
+                return this.$store.getters["form/name"];
+            },
+            set(value) {
+                this.$store.dispatch("form/setName", value);
+            },
+        },
     },
-    hypotheses: {
-      type: Object,
+    methods: {},
+    watch: {
+        // ダイアログが閉じた後フォームの値を全て空にする * computedに移行したい
+        inputForm(inputForm) {
+            if (!inputForm) {
+                this.formName = "";
+            }
+        },
     },
-    loading: {
-      type: Boolean,
-    },
-  },
-  computed: {
-    formName: {
-      get () {
-        return this.$store.getters['form/name']
-      },
-      set (value) {
-        this.$store.dispatch("form/setName", value);
-      }
-    },
- 
-  },
-  methods: {
-   
-  },
-  watch: {
-    // ダイアログが閉じた後フォームの値を全て空にする * computedに移行したい
-    inputForm(inputForm) {
-      if (!inputForm) {
-        this.formName = "";
-      }
-    },
-  },
 };
 </script>
