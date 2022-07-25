7d6c8c85717d3d472e481fa7ae569509cc5b62d5
shitogishi_yujin <hyerk.master@gmail.com>
2022-07-26 01:09:22 +0900

    変数をtodoにまとめた関係での変数変更

--- web/app/UseCases/Todo/Converter/ChildRelateToParentTodoConverter.php
+++ web/app/UseCases/Todo/Converter/ChildRelateToParentTodoConverter.php
@@ -8,25 +8,23 @@ class ChildRelateToParentTodoConverter
      * 子のTodoをここで親と紐付けて整形する
      *
      * @param array $todo_data Todo一覧のデータ
-     * @param array $childs 子のTodo
-     * @param array $parent 親のTodo
-     * @param int $len Todoの深さ
+     * @param array $todo 子のTodo
      */
-    public function invoke(array $todo_data, array $childs, array $parent, int $len)
+    public function invoke(array $todo_data, array $todo)
     {
         // 子どもに親のデータを持たせて$todo_dataに格納。
         // 親になった時にこのtodo_dataからtodoList仮説一覧配列に格納する
-        foreach ($childs as $key => $child_value) {
-            $child = $child_value->getProperties()->toArray();
+        foreach ($todo['child_todo'] as $key => $child_todo) {
+            $child = $child_todo->getProperties()->toArray();
 
             // 同じゴールの子で同じ深さの中で一番最後の子Todoか
             $child['lastChildInTheSameDepth']['lastChild'] = $key === 0 ? true : false;
 
             // 子仮説の親UUID
-            $child['parentUuid'] = $parent['uuid'];
+            $child['parentUuid'] = $todo['parent_todo']['uuid'];
 
             // ゴールからの仮説の階層の深さ
-            $child['depth'] = $len;
+            $child['depth'] = $todo['depth'];
 
             $todo_data[$child['uuid']] = $child;
         }
