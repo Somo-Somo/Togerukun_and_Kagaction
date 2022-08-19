3d1141123563699722921f3d93f1409769759854
shitogishi_yujin <hyerk.master@gmail.com>
2022-08-20 00:56:39 +0900

    DashedLine勢

---
+++ web/resources/js/components/CommonParts/Atom/DashedLine/LowerHalfDashedLine.vue
@@ -0,0 +1,20 @@
+<template>
+    <svg
+        style="style"
+        viewBox="0 0 24 28"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M12.06 0V28" stroke="black" stroke-dasharray="5 5" />
+    </svg>
+</template>
+
+<script>
+export default {
+    props: {
+        style: {
+            type: String,
+        },
+    },
+};
+</script>
