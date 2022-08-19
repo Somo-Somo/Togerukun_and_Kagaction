9e530431b1a423be2cfc96361e411bf08f51a909
shitogishi_yujin <hyerk.master@gmail.com>
2022-08-20 02:42:10 +0900

    完了ボタン

---
+++ web/resources/js/components/CommonParts/Atom/Btn/AccomplishBtn.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-btn icon :height="height" :width="width" :color="color">
+        <v-icon>mdi-checkbox-marked-circle-outline</v-icon>
+    </v-btn>
+</template>
+
+<script>
+export default {
+    props: {
+        height: {
+            type: Number,
+        },
+        width: {
+            type: Number,
+        },
+        color: {
+            type: String,
+        },
+    },
+};
+</script>
