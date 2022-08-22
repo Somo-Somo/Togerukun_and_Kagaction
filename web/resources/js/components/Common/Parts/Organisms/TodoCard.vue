3ab6c9f301b4bbb93ea2cad417ee815953376c46
shitogishi_yujin <hyerk.master@gmail.com>
2022-08-22 16:17:32 +0900

    Todoのカードリストを切り分けました。

--- web/resources/js/components/Common/Parts/Organisms/TodoCard.vue
+++ web/resources/js/components/Common/Parts/Organisms/TodoCard.vue
@@ -1,214 +1,76 @@
 <template>
-    <div>
-        <v-list class="py-0" width="100%">
-            <v-col
-                class="px-md-0"
-                v-for="todo in selectTodoList"
-                v-model="todo.showTodoList"
-                :key="todo.uuid"
-                :class="cardShow(todo) ? '' : 'd-none'"
+    <div class="d-flex">
+        <!-- 予定 -->
+        <v-card class="rounded" style="width: 100%" outlined>
+            <v-list
+                class="py-0 d-flex align-content-center"
                 :style="
-                    $vuetify.breakpoint.smAndUp
-                        ? 'padding:8px 0px'
-                        : 'padding:8px'
+                    $vuetify.breakpoint.smAndUp ? 'height:80px' : 'height:64px'
                 "
             >
-                <div class="d-flex">
-                    <!-- 予定 -->
-                    <v-card class="rounded" style="width: 100%" outlined>
-                        <v-list
-                            class="py-0 d-flex align-content-center"
-                            :style="
-                                $vuetify.breakpoint.smAndUp
-                                    ? 'height:80px'
-                                    : 'height:64px'
-                            "
-                        >
-                            <v-list-item
-                                class="px-0"
-                                style="width: 100%"
-                                @click="toTodoDetail(todo)"
-                                link
-                            >
-                                <v-list-item-action
-                                    class="d-flex px-4 ma-auto"
-                                    style="height: 24px"
-                                    @click.stop="onClickAccomplish(todo)"
-                                >
-                                    <v-btn
-                                        icon
-                                        height="24"
-                                        width="24"
-                                        :color="todo.accomplish ? 'green' : ''"
-                                    >
-                                        <v-icon
-                                            >mdi-checkbox-marked-circle-outline</v-icon
-                                        >
-                                    </v-btn>
-                                </v-list-item-action>
-                                <v-list-item-content class="pa-0 d-flex">
-                                    <div style="width: 100%">
-                                        <v-list-item-subtitle
-                                            class="d-flex align-content-start mt-3 mb-1"
-                                        >
-                                            <DateSubTitle :todo="todo" />
-                                            <div
-                                                class="d-flex"
-                                                style="max-width: 66%"
-                                            >
-                                                <p
-                                                    class="ma-0 grey--text font-weight-bold align-self-center"
-                                                    style="
-                                                        font-size: 8px;
-                                                        max-width: 100%;
-                                                        white-space: nowrap;
-                                                        overflow: hidden;
-                                                        text-overflow: ellipsis;
-                                                    "
-                                                >
-                                                    {{ parentName(todo) }}
-                                                </p>
-                                                <p
-                                                    class="ma-0 grey--text font-weight-bold align-self-center"
-                                                    style="font-size: 8px"
-                                                >
-                                                    {{ parentType(todo) }}
-                                                </p>
-                                            </div>
-                                        </v-list-item-subtitle>
-                                        <v-list-item-title class="py-2 pb-4">
-                                            <p
-                                                class="font-weight-black ma-0"
-                                                style="
-                                                    max-width: calc(
-                                                        100% - 36px
-                                                    );
-                                                    white-space: nowrap;
-                                                    overflow: hidden;
-                                                    text-overflow: ellipsis;
-                                                "
-                                                :style="
-                                                    $vuetify.breakpoint.smAndUp
-                                                        ? 'font-size:1rem'
-                                                        : 'font-size:0.8rem'
-                                                "
-                                            >
-                                                {{ todo.name }}
-                                            </p></v-list-item-title
-                                        >
-                                        <v-menu
-                                            class="rounded-lg elevation-0"
-                                            offset-y
-                                        >
-                                            <template
-                                                v-slot:activator="{ on, attrs }"
-                                            >
-                                                <v-list-item-action
-                                                    class="ma-0"
-                                                    style="
-                                                        position: absolute;
-                                                        right: 16px;
-                                                    "
-                                                    :style="
-                                                        $vuetify.breakpoint
-                                                            .smAndUp
-                                                            ? 'top: 28px;'
-                                                            : 'top: 24px;'
-                                                    "
-                                                >
-                                                    <v-btn
-                                                        v-bind="attrs"
-                                                        v-on="on"
-                                                        small
-                                                        icon
-                                                        link
-                                                    >
-                                                        <v-icon
-                                                            :size="
-                                                                $vuetify
-                                                                    .breakpoint
-                                                                    .smAndUp
-                                                                    ? '24'
-                                                                    : '20'
-                                                            "
-                                                        >
-                                                            mdi-dots-vertical
-                                                        </v-icon>
-                                                    </v-btn>
-                                                </v-list-item-action>
-                                            </template>
-                                            <v-list>
-                                                <v-list-item
-                                                    v-for="menu in cardMenu"
-                                                    :key="menu.title"
-                                                    @click="
-                                                        selectMenu(
-                                                            menu.title,
-                                                            todo
-                                                        )
-                                                    "
-                                                    link
-                                                >
-                                                    <v-list-item-title
-                                                        :style="menu.color"
-                                                        >{{
-                                                            menu.title
-                                                        }}</v-list-item-title
-                                                    >
-                                                </v-list-item>
-                                            </v-list>
-                                        </v-menu>
-                                    </div>
-                                </v-list-item-content>
-                            </v-list-item>
-                        </v-list>
-                    </v-card>
-                </div>
-            </v-col>
-            <div
-                class="my-4"
-                v-show="
-                    !todoStatus.show &&
-                    todoStatus.name !== 'ゴール' &&
-                    todoStatus.name !== 'ToDo'
-                "
-            >
-                <p
-                    class="grey--text font-weight-bold ma-0 px-4 py-2"
-                    :style="
-                        $vuetify.breakpoint.smAndUp
-                            ? 'font-size:16px;'
-                            : 'font-size:14px;'
-                    "
+                <v-list-item
+                    class="px-0"
+                    style="width: 100%"
+                    @click="toTodoDetail(todo)"
+                    link
                 >
-                    {{ todoStatus.name }}はありません
-                </p>
-            </div>
-        </v-list>
-        <deleting-confirmation-dialog
-            :deletingConfirmationDialog="deletingConfirmationDialog"
-            :selectedDeletingItemName="selectedDeletingTodo.name"
-            :loading="false"
-            @deleteItem="deleteTodo"
-            @onClickCancel="onClickCancel"
-        />
+                    <v-list-item-action
+                        class="d-flex px-4 ma-auto"
+                        style="height: 24px"
+                        @click.stop="onClickAccomplish(todo)"
+                    >
+                        <accomplish-btn
+                            height="24"
+                            width="24"
+                            color="todo.accomplish ? 'green' : ''"
+                        ></accomplish-btn>
+                    </v-list-item-action>
+                    <v-list-item-content class="pa-0 d-flex">
+                        <div style="width: 100%">
+                            <todo-title-and-sub-title
+                                :todoTitle="todo.name"
+                                :todoSubTitle="todoSubTitle"
+                            ></todo-title-and-sub-title>
+                            <v-list-item-icon
+                                class="align-self-center mx-0 px-2"
+                            >
+                                <ellipsis-menu
+                                    :menus="menus"
+                                    :selectCard="todo"
+                                    @selectedMenu="selectedMenu"
+                                ></ellipsis-menu>
+                            </v-list-item-icon>
+                        </div>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-card>
     </div>
 </template>
 
 <script>
-import DeletingConfirmationDialog from "../../Dialog/DeletingConfirmationDialog.vue";
-import DateSubTitle from "../Date/DateSubTitle.vue";
-import DeletingConfirmationDialog from "../Dialog/DeletingConfirmationDialog.vue";
+import TodoTitleAndSubTitle from "../Molecules/TodoTitleAndSubTitle.vue";
+import AccomplishBtn from "../Atom/Btn/AccomplishBtn.vue";
+import EllipsisMenu from "../../../Common/Parts/Molecules/EllipsisMenu.vue";
 
 export default {
     components: {
-        DateSubTitle,
-        DeletingConfirmationDialog,
+        TodoTitleAndSubTitle,
+        AccomplishBtn,
+        EllipsisMenu,
     },
     data: () => ({
         deletingConfirmationDialog: false,
         selectedDeletingTodo: { name: null },
         cardMenu: [{ title: "削除", color: "color: red" }],
+        date: {
+            title: null,
+            icon: "mdi-clock-outline",
+            iconSize: 14,
+            iconColor: "#212121",
+            fontColor: "#212121--text",
+            backGroundColor: "background-color: null;",
+        },
     }),
     props: {
         project: {
@@ -220,100 +82,77 @@ export default {
         todoList: {
             type: Array,
         },
-        todoStatus: {
-            type: Object,
-        },
     },
     computed: {
-        selectTodoList() {
-            return this.todoStatus.name === "予定"
-                ? this.sortScheduleList()
-                : this.todoList;
-        },
-        cardShow() {
-            return function (todo) {
-                if (this.todoStatus.name === "予定") {
-                    const today = new Date(
-                        Date.now() - new Date().getTimezoneOffset() * 60000
-                    )
-                        .toISOString()
-                        .substr(0, 10);
-                    const diff =
-                        (new Date(todo.date) - new Date(today)) /
-                        (60 * 60 * 1000 * 24);
-                    return todo.date && !(todo.accomplish && diff < 0)
-                        ? this.showTodo()
-                        : false;
-                }
-                if (this.todoStatus.name === "ToDo")
-                    return this.selectTodo.uuid === todo.parentUuid
-                        ? this.showTodo()
-                        : false;
-
-                return false;
-            };
-        },
-        parentName() {
-            return (todo) => {
-                if (todo.depth === 0) {
-                    return "「" + this.project.name;
-                } else if (todo.depth > 0) {
+        todoSubTitle() {
+            return () => {
+                if (this.todo.depth === 0) {
+                    return "「" + this.project.name + "」のゴール";
+                } else if (this.todo.depth > 0) {
                     let parentName;
                     this.todoList.map((value) => {
-                        if (todo.parentUuid === value.uuid)
+                        if (this.todo.parentUuid === value.uuid)
                             parentName = value.name;
                     });
-                    return "「" + parentName;
+                    return "「" + parentName + "」のためのToDo";
                 }
             };
         },
-        parentType() {
-            return (todo) => {
-                if (todo.depth === 0) return "」のゴール";
-                if (todo.depth > 0) return "」のためのToDo";
+        todoDate() {
+            return () => {
+                this.date.title = this.switchingDateTitle(this.todo);
+                this.date.backGroundColor = this.switchingDateBackGroundColor(
+                    this.todo
+                );
+                return this.date;
             };
         },
     },
     methods: {
-        showTodo() {
-            this.todoStatus.show = true;
-            return true;
+        switchingDateTitle(todo) {
+            const diff = this.calcDateDiff(todo);
+            if (diff > 0) {
+                return "残り" + diff + "日";
+            } else if (diff === 0) {
+                return "今日";
+            } else if (todo.accomplish) {
+                const year = new Date(todo.date).getFullYear();
+                const month = new Date(todo.date).getMonth() + 1;
+                const day = new Date(todo.date).getDay() + 1;
+                return new Date().getFullYear() === year
+                    ? month + "月" + day + "日"
+                    : year + "年" + month + "月" + day + "日";
+            } else {
+                return Math.abs(diff) + "日経過";
+            }
+        },
+        switchingDateBackGroundColor(todo) {
+            const diff = this.calcDateDiff(todo);
+            if (diff > 0) {
+                return diff < 4
+                    ? "background-color: yellow"
+                    : "background-color: transparent";
+            } else if (diff === 0) {
+                return "background-color: skyblue";
+            } else if (todo.accomplish) {
+                return "background-color: transparent;";
+            } else {
+                return "background-color: coral";
+            }
         },
         async toTodoDetail(todo) {
             await this.$store.dispatch("todo/selectTodo", todo);
             return this.$router.push({ path: "/todo/" + todo.uuid });
         },
-        selectMenu(menuTitle, todo) {
+        selectedMenu(menuTitle, todo) {
             if (menuTitle === "削除") {
                 this.deletingConfirmationDialog = true;
                 this.selectedDeletingTodo = todo;
             }
         },
-        async deleteTodo() {
-            this.deletingConfirmationDialog = false;
-            await this.$store.dispatch(
-                "todo/deleteTodo",
-                this.selectedDeletingTodo
-            );
-            this.selectedDeletingTodo = { name: null };
-        },
-        onClickCancel() {
-            this.deletingConfirmationDialog = false;
-            this.selectedDeletingTodo = { name: null };
-        },
         onClickAccomplish(todo) {
             this.$set(todo, "accomplish", todo.accomplish ? false : true);
             this.$store.dispatch("todo/updateAccomplish", todo);
-        },
-        sortScheduleList() {
-            const scheduleList = [];
-            for (const [key, todo] of Object.entries(this.todoList)) {
-                if (todo.date) scheduleList.push(todo);
-            }
-            let sortScheduleList = scheduleList.sort(function (a, b) {
-                return a.date < b.date ? -1 : 1; //オブジェクトの昇順ソート
-            });
-            return sortScheduleList;
         },
     },
     watch: {
