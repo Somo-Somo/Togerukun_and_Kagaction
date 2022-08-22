a46b8e142ef64ab4b4fe78d4f201f5205a774d2b
shitogishi_yujin <hyerk.master@gmail.com>
2022-08-22 12:17:52 +0900

    TodoTreeのTableの方完成

--- web/resources/js/components/Views/TodoList/Templates/TodoTreeTable.vue
+++ web/resources/js/components/Views/TodoList/Templates/TodoTreeTable.vue
@@ -20,48 +20,17 @@
                     @mouseout="activeLine = false"
                     link
                 >
-                    <todo-table-left-side-of-line :todo="todo" />
-                    <v-list-item-content class="px-3 py-0 ma-auto d-flex">
-                        <todo-table-item-content
-                            :project="project"
-                            :todo="todo"
-                            :todoList="todoList"
-                        />
-                    </v-list-item-content>
-                    <v-list-item-icon
-                        class="align-self-center mx-0 px-2"
-                        v-show="activeLine === index"
-                    >
-                        <menu
-                            :menus="menus"
-                            :selectCard="todo"
-                            @selectedMenu="selectedMenu"
-                        />
-                    </v-list-item-icon>
+                    <todo-table-row
+                        :project="project"
+                        :todo="todo"
+                        :todoList="todoList"
+                    ></todo-table-row>
                 </v-list-item>
-                <v-list-item
-                    class="d-flex justify-space-around px-3"
+                <additional-goal-table-row
                     v-if="todoList.length > 0"
-                    style="height: 64px"
                     @click="$emit('onClickCreate')"
-                    link
                 >
-                    <v-icon class="pa-1">mdi-plus</v-icon>
-                    <v-list-item-content class="px-2">
-                        <v-list-item-title>
-                            <p
-                                class="grey--text font-weight-bold ma-0 pa-1"
-                                :style="
-                                    $vuetify.breakpoint.smAndUp
-                                        ? 'font-size:0.9rem'
-                                        : 'font-size:0.75rem'
-                                "
-                            >
-                                ゴールを追加する
-                            </p>
-                        </v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
+                </additional-goal-table-row>
             </v-list-item-group>
         </v-card>
         <deleting-confirmation-dialog
@@ -75,21 +44,18 @@
 </template>
 
 <script>
-import TodoTableLeftSideOfLine from "./TodoTableLeftSideOfLine.vue";
-import TodoTableItemContent from "./TodoTableItemContent.vue";
-import Menu from "../Buttons/Menu.vue";
-import DeletingConfirmationDialog from "../Dialog/DeletingConfirmationDialog.vue";
+import TodoTableRow from "../Parts/TodoTableRow.vue";
+import AdditionalGoalTableRow from "../Parts/AdditionalGoalTableRow.vue";
+import DeletingConfirmationDialog from "../../../CommonParts/Molecules/DeletingConfirmationDialog.vue";
 
 export default {
     components: {
-        TodoTableLeftSideOfLine,
-        TodoTableItemContent,
-        Menu,
+        TodoTableRow,
+        AdditionalGoalTableRow,
         DeletingConfirmationDialog,
     },
     data: () => ({
         activeLine: false,
-        menus: [{ title: "削除", color: "color: red" }],
         deletingConfirmationDialog: false,
         selectedDeletingTodo: { name: null },
     }),
@@ -106,12 +72,6 @@ export default {
         async toTodoDetail(todo) {
             await this.$store.dispatch("todo/selectTodo", todo);
             return this.$router.push({ path: "/todo/" + todo.uuid });
-        },
-        selectedMenu(menuTitle, todo) {
-            if (menuTitle === "削除") {
-                this.deletingConfirmationDialog = true;
-                this.selectedDeletingTodo = todo;
-            }
         },
         onClickCancel() {
             this.deletingConfirmationDialog = false;
