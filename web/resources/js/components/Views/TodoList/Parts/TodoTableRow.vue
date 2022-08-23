24d372c6537fe9776ac648dd52311e71c23e45a6
shitogishi_yujin <hyerk.master@gmail.com>
2022-08-23 17:21:50 +0900

    タイトルとサブタイトルの横の長さ

--- web/resources/js/components/Views/TodoList/Parts/TodoTableRow.vue
+++ web/resources/js/components/Views/TodoList/Parts/TodoTableRow.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="d-flex">
+    <div class="d-flex" style="'position: relative; width: 100%;'">
         <div class="d-flex" style="max-height: 88px">
             <div class="d-flex">
                 <goal-flag v-if="todo.depth === 0" />
