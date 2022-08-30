5ea9a1bbfa876fa7f0e1b4ddb431209e89d30731
shitogishi_yujin <hyerk.master@gmail.com>
2022-08-30 15:07:09 +0900

    Cause単体で切り分ける

---
+++ web/resources/js/components/Common/Parts/Molecules/Cause.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <v-list-item-action class="px-4 ma-auto" style="height: 24px">
+            <v-icon>mdi-help-circle-outline</v-icon>
+        </v-list-item-action>
+        <div class="d-flex px-2">
+            <v-list-item-title class="py-auto">
+                <p
+                    class="font-weight-black ma-0"
+                    style="
+                        font-size: 1rem;
+                        max-width: calc(100% - 36px);
+                        white-space: nowrap;
+                        overflow: hidden;
+                        text-overflow: ellipsis;
+                    "
+                >
+                    {{ cause.text }}
+                </p></v-list-item-title
+            >
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    data: () => ({}),
+    props: {
+        cause: {
+            type: Object,
+        },
+    },
+    computed: {},
+    methods: {},
+};
+</script>
+<style scoped lang="sass">
+.v-icon.v-icon:after
+  background-color: transparent !important
+</style>
