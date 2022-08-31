f6764010d131f5cf59d3eb631879bb5a8ca8c190
shitogishi_yujin <hyerk.master@gmail.com>
2022-08-31 14:15:32 +0900

    カードの上部切り分け

---
+++ web/resources/js/components/Views/Login/Parts/LoginCardTitle.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-card-title class="d-flex text-center pa-4">
+        <div class="mx-auto">
+            <img src="/img/Kagaction_Login.svg" />
+        </div>
+        <div class="mx-auto my-0">
+            <p class="ma-0 fill-width font-weight-bold" style="font-size: 14px">
+                {{ title }}
+            </p>
+        </div>
+    </v-card-title>
+</template>
+
+<script>
+export default {
+    data: () => ({}),
+    props: {
+        title: {
+            type: String,
+        },
+    },
+};
+</script>
