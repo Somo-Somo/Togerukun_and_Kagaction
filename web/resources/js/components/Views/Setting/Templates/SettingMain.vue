99976ac8cbba008cd865a512b9e18dc93ddf4a02
shitogishi_yujin <hyerk.master@gmail.com>
2022-09-01 18:49:37 +0900

    設定のリストたち

--- web/resources/js/components/Views/Setting/Templates/SettingMain.vue
+++ web/resources/js/components/Views/Setting/Templates/SettingMain.vue
@@ -3,73 +3,34 @@
         <v-row class="d-flex justify-start ma-2">
             <user-account :user="user"></user-account>
         </v-row>
-        <v-list class="pa-0 ma-2">
-            <v-list-item-group>
-                <v-list-item
-                    v-for="(item, i) in items"
-                    :key="i"
-                    @click.native="onClickItem(item.click)"
-                >
-                    <v-list-item-icon>
-                        <v-icon v-text="item.icon"></v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title
-                            class="font-weight-bold"
-                            :class="item.color ? item.color : false"
-                            :style="
-                                $vuetify.breakpoint.smAndUp
-                                    ? 'font-size: 1rem;'
-                                    : 'font-size: 0.9rem;'
-                            "
-                            v-text="item.text"
-                        ></v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list-item-group>
-        </v-list>
+        <setting-rows
+            @onClickLogout="onClickLogout"
+            @onClickContact="onClickContact"
+        ></setting-rows>
     </div>
 </template>
 
 <script>
 import UserAccount from "../../../Common/Parts/Molecules/UserAccount.vue";
+import SettingRows from "../Parts/SettingRows.vue";
 
 export default {
     components: {
-        Header,
         UserAccount,
+        SettingRows,
     },
     data: () => ({}),
     props: {
-        apiStatus: {
-            type: Boolean,
-        },
-        isLogin: {
-            type: Boolean,
-        },
         user: {
             type: Object,
         },
     },
-    computed: {
-        initial() {
-            return this.user.name.charAt(0);
-        },
-        userId() {
-            return this.user.uuid.substr(0, 23);
-        },
-    },
+    computed: {},
     methods: {
-        onClickItem: function (click) {
-            if (click === "guide") return;
-            if (click === "contact") return this.contact();
-            if (click === "logout") return this.logout();
-            return;
-        },
-        async logout() {
+        async onClickLogout() {
             this.$emit("onClickLogout");
         },
-        contact() {
+        onClickContact() {
             this.$emit("onClickContact");
         },
     },
