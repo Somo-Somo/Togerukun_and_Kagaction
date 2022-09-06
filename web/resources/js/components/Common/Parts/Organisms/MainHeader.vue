149dacfbbf73864aca39fb90b54bf20152f2ce2a
shitogishi_yujin <hyerk.master@gmail.com>
2022-09-06 10:31:12 +0900

    コンポーネントに切り替えたよ

--- web/resources/js/components/Common/Parts/Organisms/MainHeader.vue
+++ web/resources/js/components/Common/Parts/Organisms/MainHeader.vue
@@ -15,123 +15,40 @@
             class="mt-3 hidden-sm-and-down"
         ></v-app-bar-nav-icon>
         <v-toolbar-title class="d-flex justify-start mt-3 px-0 ml-md-2">
-            <div
-                v-if="!project && !todo && !parentTodo"
-                class="d-flex align-self-center px-1"
-            >
-                <h1
-                    :style="
-                        $vuetify.breakpoint.mdAndUp
-                            ? 'font-size: 18px'
-                            : 'font-size: 1rem'
-                    "
-                >
-                    {{ headerTitle }}
-                </h1>
-            </div>
-            <div v-if="project" class="d-flex align-self-center">
-                <v-btn
-                    class="px-2"
-                    @click="onClickHeaderTitle('project', project)"
-                    text
-                >
-                    <h1
-                        :class="todo ? 'omitHeaderTitle' : ''"
-                        :style="
-                            $vuetify.breakpoint.mdAndUp
-                                ? 'font-size: 18px'
-                                : 'font-size: 16px'
-                        "
-                    >
-                        {{ project.name }}
-                    </h1>
-                </v-btn>
-            </div>
-            <div v-if="todo" class="d-flex align-self-center">
-                <h1
-                    v-if="todo.depth > 1"
-                    :style="
-                        $vuetify.breakpoint.mdAndUp
-                            ? 'font-size: 18px'
-                            : 'font-size: 16px'
-                    "
-                >
-                    /
-                </h1>
-                <h1
-                    v-if="todo.depth > 1"
-                    :style="
-                        $vuetify.breakpoint.mdAndUp
-                            ? 'font-size: 18px'
-                            : 'font-size: 16px'
-                    "
-                    class="px-2"
-                >
-                    ...
-                </h1>
-            </div>
-            <div v-if="parentTodo" class="d-flex align-self-center">
-                <h1
-                    :style="
-                        $vuetify.breakpoint.mdAndUp
-                            ? 'font-size: 18px'
-                            : 'font-size: 16px'
-                    "
-                    class="d-flex align-self-center"
-                >
-                    /
-                </h1>
-                <v-btn
-                    class="px-2"
-                    @click="onClickHeaderTitle('todo', parentTodo)"
-                    text
-                >
-                    <h1
-                        class="omitHeaderTitle"
-                        :style="
-                            $vuetify.breakpoint.mdAndUp
-                                ? 'font-size: 18px'
-                                : 'font-size: 16px'
-                        "
-                    >
-                        {{ parentTodo.name }}
-                    </h1>
-                </v-btn>
-            </div>
-            <div v-if="todo" class="d-flex align-self-center">
-                <h1
-                    :style="
-                        $vuetify.breakpoint.mdAndUp
-                            ? 'font-size: 18px'
-                            : 'font-size: 16px'
-                    "
-                    class="d-flex align-self-center"
-                >
-                    /
-                </h1>
-                <v-btn
-                    class="px-2"
-                    @click="onClickHeaderTitle('todo', todo)"
-                    text
-                >
-                    <h1
-                        class="omitHeaderTitle"
-                        :style="
-                            $vuetify.breakpoint.mdAndUp
-                                ? 'font-size: 18px'
-                                : 'font-size: 16px'
-                        "
-                    >
-                        {{ todo.name }}
-                    </h1>
-                </v-btn>
-            </div>
+            <header-title v-if="!project" :title="headerTitle"></header-title>
+            <header-title
+                v-if="project"
+                :title="project.name"
+                @onClickHeaderTitle="onClickHeaderTitle('project', project)"
+            ></header-title>
+            <header-title
+                v-if="todo.depth > 1"
+                :title="'...'"
+                :todo="todo"
+            ></header-title>
+            <header-title
+                v-if="parentTodo"
+                :title="parentTodo.name"
+                :todo="todo"
+                @onClickHeaderTitle="onClickHeaderTitle('todo', parentTodo)"
+            ></header-title>
+            <header-title
+                v-if="todo"
+                :title="todo.name"
+                :todo="todo"
+                @onClickHeaderTitle="onClickHeaderTitle('todo', todo)"
+            ></header-title>
         </v-toolbar-title>
     </v-app-bar>
 </template>
 
 <script>
+import HeaderTitle from "../Molecules/HeaderTitle.vue";
+
 export default {
+    components: {
+        HeaderTitle,
+    },
     props: {
         headerTitle: {
             type: String,
@@ -169,10 +86,4 @@ export default {
     },
 };
 </script>
-<style scoped lang="sass">
-.omitHeaderTitle
-  max-width:30vw
-  white-space: nowrap
-  overflow: hidden
-  text-overflow: ellipsis
-</style>
+<style scoped lang="sass"></style>
