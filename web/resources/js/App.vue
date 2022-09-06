fed2c0172902ea14cba6684b06526dfdba8175e6
shitogishi_yujin <hyerk.master@gmail.com>
2022-09-06 14:05:25 +0900

    存在しないファイルとコンポーネント

--- web/resources/js/App.vue
+++ web/resources/js/App.vue
@@ -3,24 +3,20 @@
         <navigation-bar
             v-if="checkPath && this.$route.path !== '/onboarding'"
         ></navigation-bar>
-        <Navbar v-if="checkPath && this.$route.path !== '/onboarding'" />
         <v-main :class="$vuetify.breakpoint.mdAndUp ? '' : 'py-0'">
             <RouterView />
         </v-main>
-        <Footer v-if="checkPath" />
     </v-app>
 </template>
 
 <script>
 import NavigationBar from "./components/Common/Template/NavigationBar.vue";
-import Footer from "./components/Footer.vue";
 import { NOT_FOUND, INTERNAL_SERVER_ERROR, UNAUTHORIZED } from "./util";
 import { mapGetters } from "vuex";
 
 export default {
     components: {
         NavigationBar,
-        Footer,
     },
     data: () => ({
         navigation: true,
