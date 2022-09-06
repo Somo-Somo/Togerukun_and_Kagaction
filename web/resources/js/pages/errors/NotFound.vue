b1e57c657ac6888dce50c31ae20621f3fc3d5225
shitogishi_yujin <hyerk.master@gmail.com>
2022-09-06 12:57:39 +0900

    エラーコンポーネント削除

--- web/resources/js/pages/errors/NotFound.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <v-alert type="error">
-      ページが見つかりませんでした。
-      <a href="/schedule" style="color:white; text-decoration: underline white;">
-        トップページへ戻る
-      </a>
-    </v-alert>
-</template>
