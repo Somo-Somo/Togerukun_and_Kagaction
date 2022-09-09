ab5e32f4e260002b63cb24d12a01f6a43371d753
shitogishi_yujin <hyerk.master@gmail.com>
2022-09-09 16:45:11 +0900

    ユーザーを登録

--- web/app/Http/Controllers/Api/LineBotController.php
+++ web/app/Http/Controllers/Api/LineBotController.php
@@ -3,20 +3,37 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Services\LineBotService;
+use App\Usecases\Line\HasRegister;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot;
 
 class LineBotController extends Controller
 {
     /**
-     * @var LineBotService
+     * @param LineBotService
      */
     protected $line_bot_service;
+
+    /**
+     * @param LINE\LINEBot\HTTPClient\CurlHTTPClient
+     */
+    protected $httpClient;
+
+    /**
+     * @param LINE\LINEBot
+     */
+    protected $bot;
 
     public function __construct()
     {
         $this->line_bot_service = new LineBotService();
+        $this->httpClient = new CurlHTTPClient(config('app.line_channel_access_token'));
+        $this->bot = new LINEBot($this->httpClient, ['channelSecret' => config('app.line_channel_secret')]);
     }
 
     /**
@@ -28,7 +45,7 @@ class LineBotController extends Controller
      *
      * @param Request
      */
-    public function reply(Request $request)
+    public function reply(Request $request, HasRegister $hasRegister)
     {
         // Requestが来たかどうか確認する
         $content = 'Request from LINE';
@@ -39,7 +56,26 @@ class LineBotController extends Controller
         $param_str
         __EOM__;
 
-        Log::debug($log_message);
+        // LINEのユーザーIDをuserIdに代入
+        $user_id = $request['events'][0]['source']['userId'];
+
+        $user = $hasRegister->invoke($user_id);
+
+        if ($user === NULL) {
+            $profile = $this->bot->getProfile($user_id)->getJSONDecodedBody();
+            // userの会員登録が行われていない場合
+            $user = User::create([
+                'name' => $profile['displayName'],
+                'uuid' => (string) Str::uuid(),
+                'line_user_id' => $user_id,
+            ]);
+
+            Log::debug($user);
+        }
+
+
+
+        Log::debug($user_id);
 
         $status_code = $this->line_bot_service->eventHandler($request);
 
