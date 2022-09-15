7fa22600236e307cab72d0a187347adaa5113df6
shitogishi_yujin <hyerk.master@gmail.com>
2022-09-15 19:28:56 +0900

    sqlから取ってくる時intに変換

--- web/app/Models/LineUsersQuestion.php
+++ web/app/Models/LineUsersQuestion.php
@@ -13,9 +13,18 @@ class LineUsersQuestion extends Model
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<string>
+     * @var array<string,integer,string>
      */
     protected $fillable = ['line_user_id', 'question_number', 'parent_uuid'];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'question_number' => 'integer'
+    ];
 
     // question_numberに対するクラス定数
     const NO_QUESTION = 0;
