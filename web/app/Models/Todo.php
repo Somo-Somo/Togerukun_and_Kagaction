3b27b530490d387e71343c4b01ecb57be869a3e4
shitogishi_yujin <hyerk.master@gmail.com>
2022-09-16 20:46:30 +0900

    複数メッセージ

--- web/app/Models/Todo.php
+++ web/app/Models/Todo.php
@@ -4,6 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\TemplateActionBuilder\DatetimePickerTemplateActionBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use Illuminate\Support\Facades\Log;
+
+use function Psy\debug;
 
 class Todo extends Model
 {
@@ -19,5 +26,34 @@ class Todo extends Model
     public static function askGoal(string $user_name, string $project_name)
     {
         return '今度は' . $user_name . 'さんが「' . $project_name . '」で達成したいゴールを教えて！';
+    }
+
+    /**
+     * Todoの期限を聞く
+     *
+     * @param string $user_name
+     * @param string $todo_name
+     * @return \LINE\LINEBot\MessageBuilder\MultiMessageBuilder()
+     */
+    public static function askTodoLimited(string $user_name, string $todo_name)
+    {
+        $title = '「' . $todo_name . '」の期日';
+        $text = 'それでは' . $user_name . 'さんはいつまでに「' . $todo_name . '」を達成したいですか?';
+        $builder = new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder();
+        $builder->add(new TextMessageBuilder($text));
+        $builder->add(
+            new TemplateMessageBuilder(
+                $title, // チャット一覧に表示される
+                new ButtonTemplateBuilder(
+                    $title, // title
+                    'いつまでに達成したいか考えてみよう！', // text
+                    null, // 画像url
+                    [
+                        new DatetimePickerTemplateActionBuilder('期日を選択', 'storeId=12345', 'date')
+                    ]
+                )
+            )
+        );
+        return $builder;
     }
 }
