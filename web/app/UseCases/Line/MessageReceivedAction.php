42a851b955ac31ffebcad90bdc78390e87956180
shitogishi_yujin <hyerk.master@gmail.com>
2022-09-20 17:51:22 +0900

    Todoを切り分ける

--- web/app/UseCases/Line/MessageReceivedAction.php
+++ web/app/UseCases/Line/MessageReceivedAction.php
@@ -3,13 +3,8 @@
 namespace App\UseCases\Line;
 
 use App\Models\User;
-use App\Models\Project;
 use App\Models\Todo;
 use App\Models\LineUsersQuestion;
-use App\Repositories\User\UserRepositoryInterface;
-use App\Repositories\Project\ProjectRepositoryInterface;
-use App\Repositories\Goal\GoalRepositoryInterface;
-use App\Repositories\Todo\TodoRepositoryInterface;
 use App\Repositories\Date\DateRepositoryInterface;
 use App\Repositories\Line\LineBotRepositoryInterface;
 use App\UseCases\Line\ProjectResponseAction;
@@ -38,21 +33,6 @@ class MessageReceivedAction
     protected $line_bot_repository;
 
     /**
-     * @param App\Repositories\Project\ProjectRepositoryInterface
-     */
-    protected $project_repository;
-
-    /**
-     * @param App\Repositories\Goal\GoalRepositoryInterface
-     */
-    protected $goal_repository;
-
-    /**
-     * @param App\Repositories\Todo\TodoRepositoryInterface
-     */
-    protected $todo_repository;
-
-    /**
      * @param App\Repositories\Todo\DateRepositoryInterface
      */
     protected $date_repository;
@@ -63,29 +43,29 @@ class MessageReceivedAction
     protected $project_response_action;
 
     /**
+     * @param App\UseCases\Line\TodoResponseAction
+     */
+    protected $todo_response_action;
+
+
+    /**
      * @param App\Repositories\Line\LineRepositoryInterface $line_repository_interface
-     * @param App\Repositories\Project\ProjectRepositoryInterface $project_repository_interface
-     * @param App\Repositories\Goal\GoalRepositoryInterface $todo_repository_interface
-     * @param App\Repositories\Todo\TodoRepositoryInterface $todo_repository_interface
      * @param App\Repositories\Date\DateRepositoryInterface $date_repository_interface
      * @param App\UseCases\LINE\ProjectResponseAction $project_response_action
+     * @param App\UseCases\LINE\TodoResponseAction $todo_response_action
      */
     public function __construct(
         LineBotRepositoryInterface $line_bot_repository_interface,
-        ProjectRepositoryInterface $project_repository_interface,
-        GoalRepositoryInterface $goal_repository_interface,
-        TodoRepositoryInterface $todo_repository_interface,
         DateRepositoryInterface $date_repository_interface,
         ProjectResponseAction $project_response_action,
+        TodoResponseAction $todo_response_action,
     ) {
         $this->httpClient = new CurlHTTPClient(config('app.line_channel_access_token'));
         $this->bot = new LINEBot($this->httpClient, ['channelSecret' => config('app.line_channel_secret')]);
         $this->line_bot_repository = $line_bot_repository_interface;
-        $this->project_repository = $project_repository_interface;
-        $this->goal_repository = $goal_repository_interface;
-        $this->todo_repository = $todo_repository_interface;
         $this->date_repository = $date_repository_interface;
         $this->project_response_action = $project_response_action;
+        $this->todo_response_action = $todo_response_action;
     }
 
     /**
@@ -112,28 +92,7 @@ class MessageReceivedAction
             $question_number === LineUsersQuestion::TODO
         ) {
             // TodoやGOALに関する質問の場合
-            $todo = [
-                'name' => $event->getText(),
-                'uuid' => (string) Str::uuid(),
-                'parent_uuid' => $line_user->question->parent_uuid,
-                'user_uuid' => $line_user->uuid
-            ];
-
-            // 返信メッセージ(日付)
-            $this->bot->replyMessage(
-                $event->getReplyToken(),
-                Todo::askTodoLimited($line_user->name, $todo['name'])
-            );
-
-            //質問の更新
-            $line_user->question->update([
-                'question_number' => LineUsersQuestion::DATE,
-                'parent_uuid' => $todo['uuid']
-            ]);
-
-            // GraphDBに保存
-            $question_number === LineUsersQuestion::GOAL ?
-                $this->goal_repository->create($todo) : $this->todo_repository->create($todo);
+            $this->todo_response_action->invoke($event, $line_user, $question_number);
         } else if ($question_number === LineUsersQuestion::DATE) {
             // 日付に関する質問の場合
             $date = [
