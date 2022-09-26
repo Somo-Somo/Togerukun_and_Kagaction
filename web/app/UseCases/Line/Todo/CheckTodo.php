6b5d9e5b9f08fe53861e7e74307de517b8c4741c
shitogishi_yujin <hyerk.master@gmail.com>
2022-09-26 12:10:32 +0900

    振り返り関係のTodo

---
+++ web/app/UseCases/Line/Todo/CheckTodo.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\UseCases\Line\Todo;
+
+use App\Models\User;
+use App\Models\Todo;
+use App\Repositories\Date\DateRepositoryInterface;
+use Illuminate\Support\Facades\Log;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot;
+use DateTime;
+
+class CheckTodo
+{
+    /**
+     * @param LINE\LINEBot\HTTPClient\CurlHTTPClient
+     */
+    protected $httpClient;
+
+    /**
+     * @param LINE\LINEBot
+     */
+    protected $bot;
+
+    /**
+     * @param App\Repositories\Date\DateRepositoryInterface
+     */
+    protected $todo_repository;
+
+    /**
+     * @param App\Repositories\Date\DateRepositoryInterface $todo_repository_interface
+     */
+    public function __construct(DateRepositoryInterface $todo_repository_interface)
+    {
+        $this->httpClient = new CurlHTTPClient(config('app.line_channel_access_token'));
+        $this->bot = new LINEBot($this->httpClient, ['channelSecret' => config('app.line_channel_secret')]);
+        $this->todo_repository = $todo_repository_interface;
+    }
+
+    /**
+     * Todoの名前を変更する
+     *
+     * @param object $event
+     * @param User $line_user
+     * @param string $todo_uuid
+     * @return
+     */
+    public function invoke(object $event, User $line_user, string $action_type, string $todo_uuid)
+    {
+
+
+
+        return;
+    }
+}
