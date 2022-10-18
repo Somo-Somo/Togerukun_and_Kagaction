9eed67053382f94d0050269659cec0ee3441929c
shitogishi_yujin <hyerk.master@gmail.com>
2022-10-18 10:31:48 +0900

    とりあえず振り返りするユーザーを探して通知を送るフォーマットまではできた

--- web/app/Models/TodoCheckNotificationDateTime.php
+++ web/app/Models/TodoCheckNotificationDateTime.php
@@ -4,26 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
-use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
-use LINE\LINEBot\TemplateActionBuilder\DatetimePickerTemplateActionBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
-use LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder;
-use LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder;
-use LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder;
-use LINE\LINEBot\ImagemapActionBuilder\AreaBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
-use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\BoxComponentBuilder;
-use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder;
-use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\BubbleContainerBuilder;
-use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\ButtonComponentBuilder;
-use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\CarouselContainerBuilder;
-use LINE\LINEBot\MessageBuilder\Flex\BlockStyleBuilder;
-use LINE\LINEBot\MessageBuilder\Flex\BubbleStylesBuilder;
-use LINE\LINEBot\MessageBuilder\FlexMessageBuilder;
-use Illuminate\Support\Facades\Log;
-use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
-use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
 class TodoCheckNotificationDateTime extends Model
 {
@@ -38,4 +18,9 @@ class TodoCheckNotificationDateTime extends Model
     const NOTIFY_TODO_CHECK = [
         'NOTIFY_TODO_CHECK' => true,
     ];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class, 'uuid', 'user_uuid');
+    }
 }
--- web/app/UseCases/Line/Todo/Notification/NotifyTodoCheck.php
+++ web/app/UseCases/Line/Todo/Notification/NotifyTodoCheck.php
@@ -2,14 +2,13 @@
 
 namespace App\UseCases\Line\Todo\Notification;
 
-use App\Models\CheckedTodo;
-use App\Models\User;
 use App\Models\Todo;
-use App\Repositories\Todo\TodoRepositoryInterface;
+use App\Models\TodoCheckNotificationDateTime;
 use App\UseCases\Line\Todo\CreateTodoListCarouselColumns as TodoCreateTodoListCarouselColumns;
-use pp\UseCases\Line\Todo\CreateTodoListCarouselColumns;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use Illuminate\Support\Facades\Log;
 use DateTime;
 
@@ -36,40 +35,45 @@ class NotifyTodoCheck
     }
 
     /**
-     * Todoの名前を変更する
+     * 該当するユーザーに振り返りの通知を送る
      *
-     * @param object $event
-     * @param User $line_user
      * @return
      */
-    public function invoke(
-        object $event,
-        User $line_user,
-        string $action_type,
-        string $notify_setting_value
-    ) {
-        $recive_notification_users = [];
+    public function invoke()
+    {
+        $datetime = new DateTime();
+        $time = $datetime->format('H') . ':00:00';
+        $day_of_week = date('w');
+        $at_this_time_user = TodoCheckNotificationDateTime::where('notification_time', $time);
+        $at_this_time_on_this_day_of_week_user = $at_this_time_user->where('notification_date', $day_of_week)->get();
+        $at_this_time_on_every_day_user = $at_this_time_user->where('notification_date', 7)->get();
+        $recive_notification_users = array_merge($at_this_time_on_this_day_of_week_user, $at_this_time_on_every_day_user);
         if (count($recive_notification_users) > 0) {
             foreach ($recive_notification_users as  $recive_notification_user) {
                 $notify_todo_check_message =
-                    '振り返りの時間です。' . "\n" . $line_user->name . 'さんが今日までにやるもの一覧です。' . "\n" . '頑張って振り返っていきましょう!';
+                    '振り返りの時間です。' . "\n" . $recive_notification_user->users->name . 'さんが今日までにやるもの一覧です。' . "\n" . '頑張って振り返っていきましょう!';
                 $today_date_time = new DateTime();
                 $today = $today_date_time->format('Y-m-d');
-                $todo_list = Todo::where('user_uuid', $line_user->uuid)
+                $todo_list = Todo::where('user_uuid', $recive_notification_user->user_uuid)
                     ->where('date', '<=', $today)
                     ->orderBy('date', 'desc')
                     ->get();
                 $create_todo_list_carousel_columns_action = new TodoCreateTodoListCarouselColumns();
                 $todo_list_carousel_flex_message = $create_todo_list_carousel_columns_action->invoke(
-                    $line_user,
+                    $recive_notification_user->users,
                     $todo_list,
-                    $action_type,
+                    $action_type = 'NOTIFY_TODO_CHECK',
                     $current_page = 1
+                );
+                $multi_message_builder = new MultiMessageBuilder();
+                $multi_message_builder->add(new TextMessageBuilder($notify_todo_check_message));
+                $multi_message_builder->add($todo_list_carousel_flex_message);
+                $this->bot->pushMessage(
+                    $recive_notification_user->user_uuid,
+                    $multi_message_builder
                 );
             }
         }
-
-
         return;
     }
 }
