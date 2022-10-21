f81c842d3d5380187801b56a735aad364616abdb
shitogishi_yujin <hyerk.master@gmail.com>
2022-10-21 12:08:08 +0900

    習慣の頻度の質問メッセージビルダー

--- web/app/Models/Habit.php
+++ web/app/Models/Habit.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder;
+use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
+
 
 class Habit extends Model
 {
@@ -51,5 +54,45 @@ class Habit extends Model
     public function todo()
     {
         return $this->belongsTo(Todo::class, 'todo_uuid', 'uuid');
+    }
+
+    /**
+     * どのくらいの頻度でやるか聞く
+     *
+     * @param string $user_name
+     * @param array $todo
+     * @return \LINE\LINEBot\MessageBuilder\MultiMessageBuilder()
+     */
+    public static function askHowOftenHabit(string $user_name, array $todo)
+    {
+        $actions = [];
+        $oftens = ['毎日', '毎週', '毎月', '平日のみ', '休日のみ'];
+
+        foreach ($oftens as $key => $often) {
+            $text_component  = new TextComponentBuilder($often, 1);
+            $text_component->setWeight('bold');
+            $text_component->setGravity('center');
+            $text_component->setAlign('center');
+            $text_component_builders = [$text_component];
+            $post_back_template_action = new PostbackTemplateActionBuilder(
+                $often,
+                'action=' . $action . '&todo_uuid=' . $parent_todo->uuid
+            );
+            $box_component = new BoxComponentBuilder('vertical', $text_component_builders);
+            $box_component->setAction($post_back_template_action);
+            $box_component->setHeight('80px');
+            $bubble_container = new BubbleContainerBuilder();
+            $bubble_container->setBody($box_component);
+            $bubble_container->setSize('nano');
+            $actions[] = $bubble_container;
+        }
+        $carousels = new CarouselContainerBuilder($actions);
+        $question_message = '「' . $parent_todo->name . '」を達成するために「やること」と「習慣」どちらを追加しますか？';
+
+        $multi_message_builder = new MultiMessageBuilder();
+        $multi_message_builder->add(new TextMessageBuilder($question_message));
+        $multi_message_builder->add(new FlexMessageBuilder($question_message, $carousels));
+
+        return $multi_message_builder;
     }
 }
