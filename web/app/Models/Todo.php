d7997d5e5a0d1cad96904c0b710a33ffacc0aac3
shitogishi_yujin <hyerk.master@gmail.com>
2022-11-09 20:58:58 +0900

    今週までにやること一覧というものを葬る

--- web/app/Models/Todo.php
+++ web/app/Models/Todo.php
@@ -340,7 +340,7 @@ class Todo extends Model
                     null, // 画像url
                     [
                         new PostbackTemplateActionBuilder('一覧を見る', 'action=ALL_TODO_LIST&page=1'),
-                        new PostbackTemplateActionBuilder('今週までにやることをみる', 'action=WEEKLY_TODO_LIST&page=1'),
+                        new PostbackTemplateActionBuilder('やることを追加する', 'action=&page=1'),
                     ]
                 )
 
