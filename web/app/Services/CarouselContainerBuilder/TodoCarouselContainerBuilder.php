8b4ab5bb6e054056ad496638e221f7c0243c8b03
shitogishi_yujin <hyerk.master@gmail.com>
2022-11-09 21:13:03 +0900

    todoカルーセル生成クラスを作りました

---
+++ web/app/Services/CarouselContainerBuilder/TodoCarouselContainerBuilder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services\CarouselContainerBuilder;
+
+use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\BoxComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\BubbleContainerBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\CarouselContainerBuilder;
+use LINE\LINEBot\MessageBuilder\FlexMessageBuilder;
+
+/**
+ * Todoのカル-セル生成クラス
+ */
+class TodoCarouselContainerBuilder
+{
+}
