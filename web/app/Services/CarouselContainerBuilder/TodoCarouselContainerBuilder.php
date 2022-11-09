b0cf1388c7db3498fb89eb591121b9a9de413a87
shitogishi_yujin <hyerk.master@gmail.com>
2022-11-09 21:21:24 +0900

    最初のtodoをカウントするバブル

--- web/app/Services/CarouselContainerBuilder/TodoCarouselContainerBuilder.php
+++ web/app/Services/CarouselContainerBuilder/TodoCarouselContainerBuilder.php
@@ -14,4 +14,38 @@ use LINE\LINEBot\MessageBuilder\FlexMessageBuilder;
  */
 class TodoCarouselContainerBuilder
 {
+    /**
+     *
+     * Todoをカウントした結果の数を表示するBubbleContainer
+     *
+     * @param string $todo_type
+     * @param int $count_todo_list
+     * @return \LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\BubbleContainerBuilder;
+     */
+    public static function createCountTodoBubbleContainer(string $todo_type, int $count_todo_list)
+    {
+        $result_count_todo_list_text = '📝' . ' ' . $count_todo_list;
+        $result_count_todo_list_text_component  = new TextComponentBuilder($result_count_todo_list_text, 1);
+        $result_count_todo_list_text_component->setGravity('bottom');
+        $result_count_todo_list_text_component->setAlign('center');
+        $result_count_todo_list_text_component->setSize('5xl');
+        $result_count_todo_list_text_component->setOffsetBottom('8px');
+
+        $report_count_todo_list_text = $todo_type . 'が' . $count_todo_list . '件見つかりました';
+        $report_count_todo_list_text_component  = new TextComponentBuilder($report_count_todo_list_text, 1);
+        $report_count_todo_list_text_component->setAlign('center');
+        $report_count_todo_list_text_component->setWeight('bold');
+        $report_count_todo_list_text_component->setWrap(true);
+
+        $texts = [
+            $result_count_todo_list_text_component,
+            $report_count_todo_list_text_component
+        ];
+        $body_box = new BoxComponentBuilder('vertical', $texts);
+        $body_box->setSpacing('lg');
+
+        $bubble_container = new BubbleContainerBuilder();
+        $bubble_container->setBody($body_box);
+        return $bubble_container;
+    }
 }
