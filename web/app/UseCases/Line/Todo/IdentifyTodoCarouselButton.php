1f3c6817f60407ebf57aa5b78915107123a16261
shitogishi_yujin <hyerk.master@gmail.com>
2022-11-11 12:47:08 +0900

    振り返るところまで成功

--- web/app/UseCases/Line/Todo/IdentifyTodoCarouselButton.php
+++ web/app/UseCases/Line/Todo/IdentifyTodoCarouselButton.php
@@ -7,6 +7,7 @@ use App\Models\Todo;
 use App\Services\ComponentBuilder\AddTodoButtonComponentBuilder;
 use App\Services\ComponentBuilder\CheckTodoButtonComponentBuilder;
 use App\Services\ComponentBuilder\ChangeAndDeleteTodoButtonComponentBuilder;
+use Illuminate\Support\Facades\Log;
 
 class IdentifyTodoCarouselButton
 {
@@ -26,12 +27,15 @@ class IdentifyTodoCarouselButton
      */
     public function invoke(Todo $todo, string $action_type)
     {
+        Log::debug($action_type);
         if ($action_type === 'ALL_TODO_LIST') {
             $actions = ChangeAndDeleteTodoButtonComponentBuilder::createChangeAndDeleteTodoComponentButton($todo);
         } else if ($action_type === 'SHOW_TODO_LIST_TO_ADD_TODO') {
             $actions = AddTodoButtonComponentBuilder::createAddTodoComponentButton($todo);
         } else if (
-            $action_type === 'CHECK_TODO_BY_TODAY' || $action_type === 'CHECK_TODO_BY_THIS_WEEK'
+            $action_type === 'CHECK_TODO_BY_TODAY'
+            || $action_type === 'CHECK_TODO_BY_THIS_WEEK'
+            || $action_type === 'SELECT_TODO_LIST_TO_CHECK'
         ) {
             $actions = CheckTodoButtonComponentBuilder::createCheckTodoComponentButton($todo);
         }
